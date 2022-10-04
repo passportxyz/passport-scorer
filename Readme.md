c52068445159e86dccd710577c2a13d88897f889
Gerald Iakobinyi-Pich <nutrina9@gmail.com>
2022-10-04 09:53:46 +0000

    Dockerizing the scorer

--- Readme.md
+++ Readme.md
@@ -1,4 +1,8 @@
 
-Start like: `gunicorn scorer.asgi:application  -k uvicorn.workers.UvicornWorker`
+# Getting Started
 
-or `uvicorn scorer.asgi:application  --reload`
+Create virtual env and install dependencies: `pipenv install`
+
+Start the dev server:
+- `gunicorn -w 4 -k uvicorn.workers.UvicornWorker scorer.asgi:application`
+- or `uvicorn scorer.asgi:application --reload`
--- scorer/settings.py
+++ scorer/settings.py
@@ -12,6 +12,13 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 
+import environ
+
+env = environ.Env(
+    # set casting, default value
+    DEBUG=(bool, False),
+)
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,16 +27,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-rbb-*4!aa&z&rt*!5hf=b-l84+5_mq^xh$*^=!4hv+@%!g)a+h"
+SECRET_KEY = env("SECRET_KEY", default="some-secret-value")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env("DEBUG", default=True)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = env.json("ALLOWED_HOSTS", default=[])
 
 
 # Application definition
-
 INSTALLED_APPS = [
     "django.contrib.admin",
     "django.contrib.auth",
@@ -83,10 +89,7 @@ WSGI_APPLICATION = "scorer.wsgi.application"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    "default": env.db(default="sqlite:////db.sqlite3"),
 }
 
 
@@ -158,3 +161,9 @@ LOGGING = {
         "level": "DEBUG",
     },
 }
+
+STATIC_ROOT = BASE_DIR / "static"
+
+print("=" * 80)
+print("ALLOWED_HOSTS", ALLOWED_HOSTS)
+print("=" * 80)
