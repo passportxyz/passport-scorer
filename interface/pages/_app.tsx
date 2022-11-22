972fa96970c49679b172cf437a3b9e1d2f747b3a
Aminah Burch <hello@aminah.io>
2022-11-22 10:34:09 +0300

    feat(app): adds initial chakra ui and tailwindcss

--- interface/pages/_app.tsx
+++ interface/pages/_app.tsx
@@ -1,6 +1,11 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { ChakraProvider } from "@chakra-ui/react";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
