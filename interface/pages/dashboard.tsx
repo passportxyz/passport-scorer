b4e8078a4c5c907ce91a1b6b22d8da8bbc96e374
Aminah Burch <hello@aminah.io>
2022-11-22 16:45:57 +0300

    feat(app): updates styling

--- interface/pages/dashboard.tsx
+++ interface/pages/dashboard.tsx
@@ -3,6 +3,7 @@ import React from "react";
 
 // --- Components
 import Header from "../components/Header";
+import { RepeatIcon } from "@chakra-ui/icons";
 
 export default function Dashboard() {
   return (
@@ -15,15 +16,18 @@ export default function Dashboard() {
           <h1 className="text-2xl font-miriamlibre">Dashboard</h1>
           <p className="mt-2 font-librefranklin text-gray-600">Generate community IDs for specific applications using non-duplication rules like first-in-first-out or last-in-first-out.</p>
         </div>
-      <div className="bg-slate-200 h-screen">
-        <div className="text-center flex flex-col justify-center align-middle">
-          <h2 className="text-xl font-miriamlibre">My Communities</h2>
-          <p className="font-librefranklin mt-2">Manage how your dapps interact with the Gitcoin Passport by creating a key that will connect to any community.</p>
-          <button className="mt-6">
-            + Create a Key
-          </button>
+        <div className="bg-slate-200 h-[45rem] flex flex-col justify-center">
+          <div className="bg-white p-2 w-13 flex text-center mx-auto rounded-full border-gray-300 border mb-8">
+            <RepeatIcon viewBox="0 0 25 25" boxSize="1.9em" color={"gray.500"} />
+          </div>
+          <div className="flex flex-col justify-center text-center align-middle mx-auto">
+            <h2 className="text-xl font-miriamlibre text-slate-600 mx-auto">My Communities</h2>
+            <p className="font-librefranklin mt-2 text-slate-600 w-9/12 mx-auto">Manage how your dapps interact with the Gitcoin Passport by creating a key that will connect to any community.</p>
+            <button className="mt-6 w-40 pt-1 pb-2 bg-slate-600 rounded-md text-white mx-auto">
+            <span className="text-xl">+</span> Create a Key
+            </button>
+          </div>
         </div>
-      </div>
       </div>
     </>
   );
--- interface/pages/index.tsx
+++ interface/pages/index.tsx
@@ -13,7 +13,7 @@ export default function Home() {
       <div>
         <Header />
       </div>
-      <div className="container mx-auto px-5 py-2">
+      <div className="container mx-auto px-5 py-2 h-full">
         <div className="mt-0 w-full pb-6 text-white sm:mt-40 sm:w-1/2 md:mt-40 md:w-1/2 md:pt-6">
           <div className="font-miriam-libre leading-relaxed">
             <img src="/assets/gitcoinWordLogo.svg" alt="Passport Logo" className="py-4 px-1" />
@@ -22,12 +22,12 @@ export default function Home() {
           <div className="font-libre-franklin mt-0 text-lg text-gray-400 sm:text-xl md:mt-10 md:pr-20 md:text-xl">
           Gitcoin Passport is an identity protocol that proves your trustworthiness without needing to collect personally identifiable information.
           </div>
-          <div className="mt-4 hidden w-full sm:mt-10 sm:w-1/2 md:mt-10 md:block md:w-1/2">
+          <div className="mt-4 w-full sm:mt-10 sm:w-1/2 md:mt-10 md:block md:w-1/2">
             <ConnectButton label="Connect Wallet" />
           </div>
         </div>
       </div>
-      <div>
+      <div className="mt-12">
         <Footer />
       </div>
     </>
