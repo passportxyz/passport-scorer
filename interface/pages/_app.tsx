d12c76d92694be1a1ce837fb3fdf3bdf2f16f704
nutrina <nutrina9@gmail.com>
2022-11-23 11:08:51 +0200

    feat(siwe): linking the UI to the authentication in the BE

--- interface/pages/_app.tsx
+++ interface/pages/_app.tsx
@@ -1,3 +1,6 @@
+// --- React Methods
+import React, { useState } from "react";
+
 // --- Styling & UI
 import "../styles/globals.css";
 import "@rainbow-me/rainbowkit/styles.css";
@@ -9,6 +12,9 @@ import {
   getDefaultWallets,
   RainbowKitProvider,
   lightTheme,
+  ConnectButton,
+  RainbowKitAuthenticationProvider,
+  AuthenticationStatus,
 } from "@rainbow-me/rainbowkit";
 import { RainbowKitSiweNextAuthProvider } from "@rainbow-me/rainbowkit-siwe-next-auth";
 
@@ -17,24 +23,59 @@ import type { AppProps } from "next/app";
 import Head from "next/head";
 
 // --- Wagmi
-import {
-  chain,
-  configureChains,
-  createClient,
-  WagmiConfig
-} from "wagmi";
-import {
-  metaMaskWallet,
-} from "@rainbow-me/rainbowkit/wallets";
+import { chain, configureChains, createClient, WagmiConfig } from "wagmi";
+import { metaMaskWallet } from "@rainbow-me/rainbowkit/wallets";
 import { alchemyProvider } from "wagmi/providers/alchemy";
 import { publicProvider } from "wagmi/providers/public";
 
+import { createAuthenticationAdapter } from "@rainbow-me/rainbowkit";
+import { SiweMessage } from "siwe";
+
+const SCORER_BACKEND = "http://localhost:8000/";
+
+const authenticationAdapter = createAuthenticationAdapter({
+  getNonce: async () => {
+    const response = await fetch(`${SCORER_BACKEND}account/nonce`);
+    return await response.text();
+  },
+
+  createMessage: ({ nonce, address, chainId }) => {
+    return new SiweMessage({
+      domain: window.location.host,
+      address,
+      statement: "Sign in with Ethereum to the app.",
+      uri: window.location.origin,
+      version: "1",
+      chainId,
+      nonce,
+    });
+  },
+
+  getMessageBody: ({ message }) => {
+    return message.prepareMessage();
+  },
+
+  verify: async ({ message, signature }) => {
+    const verifyRes = await fetch(`${SCORER_BACKEND}account/verify`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ message, signature }),
+    });
+
+    return Boolean(verifyRes.ok);
+  },
+
+  signOut: async () => {
+    await fetch("/api/logout");
+  },
+});
+
 const { chains, provider, webSocketProvider } = configureChains(
+  [chain.mainnet],
   [
-    chain.mainnet,
-  ],
-  [
-    alchemyProvider({ apiKey: process.env.NEXT_PUBLIC_PASSPORT_SCORER_ALCHEMY_API_KEY || "" }),
+    alchemyProvider({
+      apiKey: process.env.NEXT_PUBLIC_PASSPORT_SCORER_ALCHEMY_API_KEY || "",
+    }),
     publicProvider(),
   ]
 );
@@ -52,9 +93,7 @@ const connectors = connectorsForWallets([
   ...wallets,
   {
     groupName: "Wallets",
-    wallets: [
-      metaMaskWallet({ chains })
-    ],
+    wallets: [metaMaskWallet({ chains })],
   },
 ]);
 
@@ -66,6 +105,13 @@ const wagmiClient = createClient({
 });
 
 export default function App({ Component, pageProps }: AppProps) {
+  // You'll need to resolve AUTHENTICATION_STATUS here
+  // using your application's authentication system.
+  // It needs to be either 'loading' (during initial load),
+  // 'unathenticated' or 'authenticated'.
+  const [authenticationStatus, setAuthenticationStatus] =
+    useState<AuthenticationStatus>("unauthenticated");
+
   return (
     <>
       <Head>
@@ -73,22 +119,27 @@ export default function App({ Component, pageProps }: AppProps) {
         <title>Passport Scorer</title>
       </Head>
       <WagmiConfig client={wagmiClient}>
-        <RainbowKitProvider
-          appInfo={passportScorerApp}
-          chains={chains}
-          modalSize="compact"
-          theme={lightTheme({
-            accentColor: "#757087",
-            accentColorForeground: "white",
-            borderRadius: "small",
-            overlayBlur: "small",
-            fontStack: undefined,
-          })}
+        <RainbowKitAuthenticationProvider
+          adapter={authenticationAdapter}
+          status={authenticationStatus}
         >
-          <ChakraProvider>
-            <Component {...pageProps} />
-          </ChakraProvider>
-        </RainbowKitProvider>
+          <RainbowKitProvider
+            appInfo={passportScorerApp}
+            chains={chains}
+            modalSize="compact"
+            theme={lightTheme({
+              accentColor: "#757087",
+              accentColorForeground: "white",
+              borderRadius: "small",
+              overlayBlur: "small",
+              fontStack: undefined,
+            })}
+          >
+            <ChakraProvider>
+              <Component {...pageProps} />
+            </ChakraProvider>
+          </RainbowKitProvider>
+        </RainbowKitAuthenticationProvider>
       </WagmiConfig>
     </>
   );
