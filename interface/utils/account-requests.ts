821c0f6415cb2f4ae8fe2ed8c2fdce142cb6b199
schultztimothy <schultz.timothy52@gmail.com>
2022-12-01 18:06:00 -0700

    feat(interface): add community requests

--- interface/utils/account-requests.ts
+++ interface/utils/account-requests.ts
@@ -6,7 +6,7 @@ export const createApiKey = async (name: string) => {
     const token = localStorage.getItem("access-token");
     const response = await axios.post(
       `${SCORER_BACKEND}account/api-key`,
-      { data: { name } },
+      { name },
       {
         headers: {
           "Content-Type": "application/json",
@@ -30,6 +30,48 @@ export const getApiKeys = async (): Promise<ApiKeys[]> => {
     const SCORER_BACKEND = process.env.NEXT_PUBLIC_PASSPORT_SCORER_BACKEND;
     const token = localStorage.getItem("access-token");
     const response = await axios.get(`${SCORER_BACKEND}account/api-key`, {
+      headers: {
+        "Content-Type": "application/json",
+        AUTHORIZATION: `Bearer ${token}`,
+      },
+    });
+
+    const { data } = response;
+    return data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export type Community = {
+  name: string;
+  description: string;
+};
+
+export const createCommunity = async (community: Community) => {
+  try {
+    const SCORER_BACKEND = process.env.NEXT_PUBLIC_PASSPORT_SCORER_BACKEND;
+    const token = localStorage.getItem("access-token");
+    const response = await axios.post(
+      `${SCORER_BACKEND}account/communities`,
+      { ...community },
+      {
+        headers: {
+          "Content-Type": "application/json",
+          AUTHORIZATION: `Bearer ${token}`,
+        },
+      }
+    );
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const getCommunities = async (): Promise<Community[]> => {
+  try {
+    const SCORER_BACKEND = process.env.NEXT_PUBLIC_PASSPORT_SCORER_BACKEND;
+    const token = localStorage.getItem("access-token");
+    const response = await axios.get(`${SCORER_BACKEND}account/communities`, {
       headers: {
         "Content-Type": "application/json",
         AUTHORIZATION: `Bearer ${token}`,
