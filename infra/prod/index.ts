6b8ed0c1ffbb5f58735367db15b666c46694ecd3
nutrina <nutrina9@gmail.com>
2022-12-13 23:35:07 +0200

    fix(infra): fix pulumi infra definition

--- infra/prod/index.ts
+++ infra/prod/index.ts
@@ -377,7 +377,7 @@ const secgrp = new aws.ec2.SecurityGroup(`scorer-run-migrations-task`, {
 export const securityGroupForTaskDefinition = secgrp.id;
 
 //////////////////////////////////////////////////////////////
-// Set up EC2 instance 
+// Set up EC2 instance
 //      - it is intended to be used for troubleshooting
 //////////////////////////////////////////////////////////////
 
@@ -413,7 +413,7 @@ apt-get install -y \
   curl \
   gnupg \
   lsb-release
-  
+
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 
 echo \
@@ -421,7 +421,7 @@ echo \
 $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 apt-get update
-apt-get install -y docker-ce docker-ce-cli containerd.io
+apt-get install -y docker-ce docker-ce-cli containerd.io awscli
 mkdir /var/log/gitcoin
 echo $(date) "Finished installation of docker" >> /var/log/gitcoin/init.log
 
