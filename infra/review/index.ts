c3e73cbe57ec848059b4a4758c78c09484c9125f
nutrina <nutrina9@gmail.com>
2022-12-14 09:38:58 +0200

    fix(infra): install awscli in troubleshooting ec2 instance

--- infra/review/index.ts
+++ infra/review/index.ts
@@ -421,7 +421,7 @@ echo \
 $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 apt-get update
-apt-get install -y docker-ce docker-ce-cli containerd.io
+apt-get install -y docker-ce docker-ce-cli containerd.io awscli
 mkdir /var/log/gitcoin
 echo $(date) "Finished installation of docker" >> /var/log/gitcoin/init.log
 
