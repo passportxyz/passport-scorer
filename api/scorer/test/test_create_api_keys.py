3836d1d442279e9cd42a93931cf1e3cc057a353f
Aminah Burch <hello@aminah.io>
2022-12-16 14:14:46 +0300

    chore(api): updates create api keys acceptance test

--- api/scorer/test/test_create_api_keys.py
+++ api/scorer/test/test_create_api_keys.py
@@ -2,14 +2,11 @@
 import json
 
 import pytest
-from account.models import Account, AccountAPIKey
+from account.models import AccountAPIKey
 from account.test.test_api_key import mock_api_key_body
-from django.contrib.auth.models import User
 from django.test import Client
-from eth_account.messages import encode_defunct
 from ninja_jwt.schema import RefreshToken
 from pytest_bdd import given, scenario, then, when
-from registry.test.test_passport_submission import mock_passport
 from web3 import Web3
 
 web3 = Web3()
@@ -36,7 +33,6 @@ def _(scorer_account, mocker):
 def _(scorer_user, mocker):
     """I hit the Create API key button."""
 
-    mocker.patch("account.views.create_api_key", return_value=mock_api_key_body)
     refresh = RefreshToken.for_user(scorer_user)
     access_token = refresh.access_token
 
@@ -66,38 +62,19 @@ def _():
 
 
 @then("I can use that key to call the API")
-def _(scorer_api_key, scorer_community_with_gitcoin_default, mocker):
+def _(scorer_api_key, scorer_community_with_gitcoin_default):
     """I can use that key to call the API."""
-
-    mocker.patch("registry.api.get_passport", return_value=mock_passport)
-    mocker.patch("registry.api.validate_credential", side_effect=[[], []])
 
     client = Client()
 
-    my_mnemonic = (
-        "chief loud snack trend chief net field husband vote message decide replace"
-    )
-    web3_account = web3.eth.account.from_mnemonic(
-        my_mnemonic, account_path="m/44'/60'/0'/0/0"
-    )
-
-    signed_message = web3.eth.account.sign_message(
-        encode_defunct(text="I authorize the passport scorer to validate my account"),
-        private_key=web3_account.key,
-    )
-
-    payload = {
-        "community": scorer_community_with_gitcoin_default.id,
-        "address": scorer_community_with_gitcoin_default.account.address,
-        "signature": signed_message.signature.hex(),
-    }
-
-    response = client.post(
-        "/api/registry/submit-passport",
-        json.dumps(payload),
+    response = client.get(
+        f"/api/registry/score/{scorer_community_with_gitcoin_default.id}/{scorer_community_with_gitcoin_default.account.address}",
         content_type="application/json",
         HTTP_AUTHORIZATION=f"Token {scorer_api_key}",
     )
 
     assert response.status_code == 200
-    return response
+    assert response.json() == {
+        "address": "0xb81c935d01e734b3d8bb233f5c4e1d72dbc30f6c",
+        "score": "1001234.000000000",
+    }
