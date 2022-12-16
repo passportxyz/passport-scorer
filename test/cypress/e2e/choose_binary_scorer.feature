500b3c4fde67d373c4ed5398c4035188054e86d1
schultztimothy <schultz.timothy52@gmail.com>
2022-12-15 18:25:48 -0700

    feat(interface): integration test for #778

--- test/cypress/e2e/choose_binary_scorer.feature
+++ test/cypress/e2e/choose_binary_scorer.feature
@@ -1,5 +1,6 @@
+
 Feature: Choose Binary Scorer (#778)
   Scenario: I want to choose the Gitcoin Binary Community Score
-    GIVEN that I'm a developer and have setup a community
-    WHEN I see the scoring options I can select for that community
-    THEN I see the Gitcoin Binary Community Score as an option, i.e., the binary output version of the of the default Gitcoin Community Score
+    Given that I'm a developer and have setup a community
+    When I see the scoring options I can select for that community
+    Then I see the Gitcoin Binary Community Score as an option, i.e., the binary output version of the of the default Gitcoin Community Score
--- test/cypress/e2e/choose_binary_scorer.ts
+++ test/cypress/e2e/choose_binary_scorer.ts
@@ -2,15 +2,28 @@ import { Given, When, Then } from "@badeball/cypress-cucumber-preprocessor";
 import "../support/commands";
 
 Given("that I'm a developer and have setup a community", () => {
-  cy.visit("http://localhost:3000/");
   cy.siwe();
+  cy.visit("http://localhost:3000/");
+  cy.get("button").contains("Connect Wallet").click();
+  cy.visit("http://localhost:3000/dashboard");
+  cy.get("button").contains("Add").click();
+  cy.get('[data-testid="community-name-input"]').type("Test Community");
+  cy.get('[data-testid="community-description-input"]').type(
+    "Test Description"
+  );
+  cy.get('[data-testid="create-button"]').click();
 });
 
 When("I see the scoring options I can select for that community", () => {
-  cy.get("button").contains("Communities").click();
+  cy.get("a").contains("Test Community").click();
 });
 
 Then(
   "I see the Gitcoin Binary Community Score as an option, i.e., the binary output version of the of the default Gitcoin Community Score",
-  () => {}
+  () => {
+    cy.get("p").contains("Gitcoin Scoring");
+    cy.get("p").contains(
+      "Stamps and data are binarily verified, aggregated, and scored relative to all other attestations."
+    );
+  }
 );
