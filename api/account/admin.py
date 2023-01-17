49701ec4c23252c21df933dd155e6afeb12ee7b4
Lucian Hymer <lucian@lucianhymer.com>
2023-01-17 10:14:11 -0800

    feat(api): Admin updates closes issue 824

--- api/account/admin.py
+++ api/account/admin.py
@@ -5,14 +5,18 @@ from .models import Account, AccountAPIKey, Community
 
 class AccountAdmin(admin.ModelAdmin):
     list_display = ("id", "address", "user")
+    search_fields = ("address", "user__username")
 
 
 class CommunityAdmin(admin.ModelAdmin):
     list_display = ("id", "name", "description", "account")
+    raw_id_fields = ("account", "scorer")
+    search_fields = ("name", "description", "account__address")
 
 
 class AccountAPIKeyAdmin(admin.ModelAdmin):
     list_display = ("id", "name", "prefix", "created", "expiry_date", "revoked")
+    search_fields = ("id", "name", "prefix")
 
 
 admin.site.register(Account, AccountAdmin)
