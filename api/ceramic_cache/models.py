57f2feccecd36169ba26897efad2689ca3ba8776
schultztimothy <schultz.timothy52@gmail.com>
2023-01-26 18:01:48 -0700

    feat(api): rename passport to stamp

--- api/ceramic_cache/models.py
+++ api/ceramic_cache/models.py
@@ -1,17 +1,15 @@
+"""Ceramic Cache Models"""
+
 from account.models import EthAddressField
 from django.db import models
 
-"""Ceramic Cache Models"""
-
 
 class CeramicCache(models.Model):
-    """Ceramic Cache Model"""
-
     address = EthAddressField(null=True, blank=False, max_length=100)
     provider = models.CharField(
         null=False, blank=False, default="", max_length=256, db_index=True
     )
-    passport = models.JSONField(default=dict)
+    stamp = models.JSONField(default=dict)
 
     class Meta:
         unique_together = ["address", "provider"]
