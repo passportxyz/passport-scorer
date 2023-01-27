1552bbb0ebf4fcd317ea41444a99ca29058c3254
schultztimothy <schultz.timothy52@gmail.com>
2023-01-27 12:04:09 -0700

    feat(api): add deleted_at column to ceramic cache

--- api/ceramic_cache/models.py
+++ api/ceramic_cache/models.py
@@ -5,11 +5,12 @@ from django.db import models
 
 
 class CeramicCache(models.Model):
-    address = EthAddressField(null=True, blank=False, max_length=100)
+    address = EthAddressField(null=True, blank=False, max_length=100, db_index=True)
     provider = models.CharField(
         null=False, blank=False, default="", max_length=256, db_index=True
     )
     stamp = models.JSONField(default=dict)
+    deleted_at = models.BooleanField(default=False)
 
     class Meta:
         unique_together = ["address", "provider"]
