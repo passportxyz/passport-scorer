59288e3d558db58ade7f47a319456f551efe4606
nutrina <nutrina9@gmail.com>
2023-02-03 16:15:38 +0200

    feat(db_cache): adding JST authentication class (but not setting it yet on requests) for ceramic_cache)

--- api/ceramic_cache/api.py
+++ api/ceramic_cache/api.py
@@ -2,18 +2,31 @@
 
 import logging
 from datetime import datetime, timedelta
-from typing import Any, Dict, List
+from typing import Any, Dict, List, Type
 
 import requests
 from django.conf import settings
 from ninja import Router, Schema
-from ninja.security import APIKeyHeader
 from ninja_extra import status
 from ninja_extra.exceptions import APIException
 from ninja_jwt.tokens import RefreshToken
 
 from .exceptions import InvalidDeleteCacheRequestException
 from .models import CeramicCache
+
+from django.conf import settings
+from django.contrib.auth import get_user_model
+from ninja import Schema
+from ninja_extra import status
+from ninja_extra.exceptions import APIException
+from ninja_jwt.schema import RefreshToken
+from ninja_schema import Schema
+from ninja_jwt.tokens import Token, TokenError
+from ninja_jwt.authentication import InvalidToken
+from django.contrib.auth.models import AbstractUser
+from ninja_extra.security import HttpBearer
+from django.http import HttpRequest
+from ninja_jwt.settings import api_settings
 
 log = logging.getLogger(__name__)
 
@@ -22,6 +35,59 @@ router = Router()
 
 def get_utc_time():
     return datetime.utcnow()
+
+
+class JWTDidAuthentication:
+    """
+    This authentication class will validate an access token that contains a claim named `did`
+    and will save that value in `request.did`
+    """
+
+    def __init__(self) -> None:
+        super().__init__()
+        self.user_model = get_user_model()
+
+    @classmethod
+    def get_validated_token(cls, raw_token) -> Type[Token]:
+        """
+        Validates an encoded JSON web token and returns a validated token
+        wrapper object.
+        """
+        messages = []
+        for AuthToken in api_settings.AUTH_TOKEN_CLASSES:
+            try:
+                ret = AuthToken(raw_token)
+                return ret
+            except TokenError as e:
+                messages.append(
+                    {
+                        "token_class": AuthToken.__name__,
+                        "token_type": AuthToken.token_type,
+                        "message": e.args[0],
+                    }
+                )
+
+        raise InvalidToken(
+            {
+                "detail": _("Given token not valid for any token type"),
+                "messages": messages,
+            }
+        )
+
+    def jwt_authenticate(self, request: HttpRequest, token: str) -> Type[AbstractUser]:
+        request.did = None
+        validated_token = self.get_validated_token(token)
+        request.did = validated_token["did"]
+        return request.did
+
+
+class JWTDidAuth(JWTDidAuthentication, HttpBearer):
+    """
+    This authentication class to check for did claim in the token
+    """
+
+    def authenticate(self, request: HttpRequest, token: str) -> Any:
+        return self.jwt_authenticate(request, token)
 
 
 class CacheStampPayload(Schema):
@@ -55,6 +121,7 @@ class GetStampResponse(Schema):
 @router.post(
     "stamp",
     response={201: CachedStampResponse},
+    # auth=JWTDidAuth()    <-- to be uncommented in order to enable authentication
 )
 def cache_stamp(_, payload: CacheStampPayload):
     try:
@@ -74,6 +141,7 @@ def cache_stamp(_, payload: CacheStampPayload):
 @router.delete(
     "stamp",
     response=DeleteStampResponse,
+    # auth=JWTDidAuth()    <-- to be uncommented in order to enable authentication
 )
 def soft_delete_stamp(_, payload: DeleteStampPayload):
     try:
@@ -93,10 +161,7 @@ def soft_delete_stamp(_, payload: DeleteStampPayload):
         raise InvalidDeleteCacheRequestException()
 
 
-@router.get(
-    "stamp",
-    response=GetStampResponse,
-)
+@router.get("stamp", response=GetStampResponse)
 def get_stamps(_, address):
     try:
         stamps = CeramicCache.objects.filter(deleted_at=None, address=address)
@@ -141,6 +206,10 @@ class AcessTokenResponse(Schema):
     response=AcessTokenResponse,
 )
 def authenticate(request, payload: CacaoVerifySubmit):
+    """
+    This method will validat a jws created with DagJWS, will validate by forwarding this to our validator
+    it and return a JWT token.
+    """
     try:
         r = requests.post(
             settings.CERAMIC_CACHE_CACAO_VALIDATION_URL, json=payload.dict()
