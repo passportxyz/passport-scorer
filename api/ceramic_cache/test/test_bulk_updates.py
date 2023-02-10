32498ca38bd02594b9327d12c47c51b3261541b3
schultztimothy <schultz.timothy52@gmail.com>
2023-02-09 18:59:39 -0700

    feat(api): test bulk update

--- api/ceramic_cache/test/test_bulk_updates.py
+++ api/ceramic_cache/test/test_bulk_updates.py
@@ -12,25 +12,23 @@ client = Client()
 
 class TestBulkStampUpdates:
     def test_bulk_update(
-        self, sample_providers, sample_addresses, sample_stamps, api_key
+        self, sample_providers, sample_address, sample_stamps, sample_token
     ):
         bulk_payload = []
         for i in range(0, len(sample_providers)):
             bulk_payload.append(
                 {
-                    "address": sample_addresses[i],
+                    "address": sample_address,
                     "provider": sample_providers[i],
                     "stamp": sample_stamps[i],
                 }
             )
 
-        params = {"stamps": bulk_payload}
-
         cache_stamp_response = client.post(
             "/ceramic-cache/stamps",
-            json.dumps(params),
+            json.dumps(bulk_payload),
             content_type="application/json",
-            **{"HTTP_X-API-Key": api_key},
+            **{"HTTP_AUTHORIZATION": f"Bearer {sample_token}"},
         )
 
         assert cache_stamp_response.status_code == 201
