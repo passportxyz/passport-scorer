fd1c8e46bed876a8d04fa8d108910648eeb74f20
nutrina <nutrina9@gmail.com>
2023-02-15 10:24:00 +0200

    feat(api, docs): adding tag for scorer API

--- api/scorer/api.py
+++ api/scorer/api.py
@@ -8,7 +8,7 @@ from registry.api import analytics_router
 from registry.api import router as registry_router
 
 registry_api = NinjaAPI(urls_namespace="registry", title="Scorer API")
-registry_api.add_router("/registry/", registry_router)
+registry_api.add_router("/registry/", registry_router, tags=["Score your passport"])
 
 ceramic_cache_api = NinjaAPI(urls_namespace="ceramic-cache", docs_url=None)
 ceramic_cache_api.add_router("", ceramic_cache_router)
