d361509cd257a2d047f5a237595708f6153741a2
nutrina <nutrina9@gmail.com>
2023-02-21 09:43:22 +0200

    fix(api): remove unused code

--- api/scorer_weighted/computation.py
+++ api/scorer_weighted/computation.py
@@ -8,40 +8,20 @@ from scorer_weighted.models import WeightedScorer
 log = logging.getLogger(__name__)
 
 
-# pylint: disable=unused-argument
-def calculate_score(sender, passport_ids, **kwargs):
-    """
-    Calculate the score for the given list of passport IDs.
-    
-    This function retrieves the weighted scorers that are active at the time of
-    the function call and calculates the weighted score for each passport ID
-    in the input list by calling `calculate_weighted_score`.
-    
-    Args:
-        sender: The sender of the signal that triggered the function call.
-        passport_ids (List[int]): A list of passport IDs to calculate the score for.
-        **kwargs: Additional keyword arguments that are ignored.
-    """
-    now = datetime.now()
-    log.debug("calculate_score has been triggered for %s", passport_ids)
-    for scorer in WeightedScorer.objects.filter(start_time__lte=now, end_time__gt=now):
-        calculate_weighted_score(scorer, passport_ids)
-
-
 def calculate_weighted_score(
     scorer: WeightedScorer, passport_ids: List[int]
 ) -> List[Decimal]:
     """
     Calculate the weighted score for the given list of passport IDs and a single scorer.
-    
+
     This function retrieves the weights for the scorer, filters the stamps associated
     with each passport ID, and calculates the weighted score based on the weights of
     the stamps. The weight of each stamp is determined by the scorer's weights dict.
-    
+
     Args:
         scorer (WeightedScorer): The scorer to use for calculating the weighted score.
         passport_ids (List[int]): A list of passport IDs to calculate the weighted score for.
-    
+
     Returns:
         A list of Decimal values representing the weighted scores for the given passport IDs.
     """
