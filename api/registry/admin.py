3aff97032f1a03373c69eff48bad368fa857233b
nutrina <nutrina9@gmail.com>
2023-02-22 14:13:20 +0200

    feat(api,admin): render links to the community in the registry instances

--- api/registry/admin.py
+++ api/registry/admin.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
+from django.urls import reverse
+from django.utils.html import mark_safe
 from registry.models import Passport, Score, Stamp
 
 
 class PassportAdmin(admin.ModelAdmin):
-    list_display = ["address", "community"]
+    list_display = ["address", "community_link"]
     search_fields = ["address"]
 
     def get_queryset(self, request):
@@ -11,14 +13,44 @@ class PassportAdmin(admin.ModelAdmin):
         queryset = queryset.prefetch_related("community")
         return queryset
 
+    @admin.display(description="Community")
+    def community_link(self, obj):
+        if obj.community:
+            community = obj.community
+            community_url = reverse(
+                f"admin:{community._meta.app_label}_{community._meta.model_name}_change",
+                args=(community.pk,),
+            )
+            display_text = f'<a href="{community_url}">{community.name}</a>'
+
+            if display_text:
+                return mark_safe(display_text)
+
+        return "-"
+
+
+PassportAdmin.community_link.verbose = "dddd"
+
 
 class StampAdmin(admin.ModelAdmin):
     list_display = ["passport", "community", "provider", "hash"]
     search_fields = ["passport__address", "provider", "hash"]
     raw_id_fields = ["passport"]
 
+    @admin.display(description="Community")
     def community(self, obj):
-        return obj.passport.community
+        if obj.passport and obj.passport.community:
+            community = obj.passport.community
+            community_url = reverse(
+                f"admin:{community._meta.app_label}_{community._meta.model_name}_change",
+                args=(community.pk,),
+            )
+            display_text = f'<a href="{community_url}">{community.name}</a>'
+
+            if display_text:
+                return mark_safe(display_text)
+
+        return "-"
 
     def get_queryset(self, request):
         queryset = super().get_queryset(request)
@@ -38,8 +70,20 @@ class ScoreAdmin(admin.ModelAdmin):
     search_fields = ["passport__address", "score", "status", "error"]
     raw_id_fields = ["passport"]
 
+    @admin.display(description="Community")
     def community(self, obj):
-        return obj.passport.community
+        if obj.passport and obj.passport.community:
+            community = obj.passport.community
+            community_url = reverse(
+                f"admin:{community._meta.app_label}_{community._meta.model_name}_change",
+                args=(community.pk,),
+            )
+            display_text = f'<a href="{community_url}">{community.name}</a>'
+
+            if display_text:
+                return mark_safe(display_text)
+
+        return "-"
 
     def get_queryset(self, request):
         queryset = super().get_queryset(request)
