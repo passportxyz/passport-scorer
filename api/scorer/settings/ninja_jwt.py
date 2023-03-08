807d233f087142a800fa1d954434daae23c37db0
schultztimothy <schultz.timothy52@gmail.com>
2023-03-08 11:12:26 -0700

    fix(api): include ninja settings and fix timedelta

--- api/scorer/settings/ninja_jwt.py
+++ api/scorer/settings/ninja_jwt.py
@@ -1,9 +1,10 @@
+"""Ninja JWT settings."""
 from datetime import timedelta
 
 from .env import env
 
 NINJA_JWT = {
-    "REFRESH_TOKEN_LIFETIME": env.timedelta(
-        minutes=env.int("SCORER_UI_REFRESH_TOKEN_LIFETIME", default=1440)
+    "REFRESH_TOKEN_LIFETIME": timedelta(
+        minutes=env.int("SCORER_UI_REFRESH_TOKEN_LIFETIME", default=10440)
     ),
 }
