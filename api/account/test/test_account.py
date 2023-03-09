d12b50143e78b8ac71e841f21e6d73a3310e65f1
schultztimothy <schultz.timothy52@gmail.com>
2023-03-08 17:32:07 -0700

    fix(api): remove unnecesarry www client

--- api/account/test/test_account.py
+++ api/account/test/test_account.py
@@ -23,6 +23,8 @@ web3 = Web3()
 web3.eth.account.enable_unaudited_hdwallet_features()
 account = web3.eth.account.from_mnemonic(my_mnemonic, account_path="m/44'/60'/0'/0/0")
 
+c = Client()
+
 
 class AccountTestCase(TestCase):
     def setUp(self):
@@ -30,8 +32,6 @@ class AccountTestCase(TestCase):
 
     def test_create_account_with_SIWE(self):
         """Test creation of an account wit SIWE"""
-
-        c = Client()
         response = c.get("/account/nonce")
         self.assertEqual(200, response.status_code)
 
@@ -79,20 +79,7 @@ class AccountTestCase(TestCase):
         self.assertTrue("access" in data)
 
     def test_create_account_with_www_domain(self):
-        class WwwClient(Client):
-            def get(self, path, data=None, **extra):
-                # Add "www" to the HTTP_HOST header
-                extra.setdefault("HTTP_HOST", "www.example.com")
-                return super().get(path, data, **extra)
-
-            def post(self, path, data=None, **extra):
-                # Add "www" to the HTTP_HOST header
-                extra.setdefault("HTTP_HOST", "www.example.com")
-                return super().post(path, data, **extra)
-
-        www_client = WwwClient()
-
-        response = www_client.get("/account/nonce")
+        response = c.get("/account/nonce")
         self.assertEqual(200, response.status_code)
 
         data = response.json()
@@ -120,7 +107,7 @@ class AccountTestCase(TestCase):
             encode_defunct(text=data_to_sign), private_key=private_key
         )
 
-        response = www_client.post(
+        response = c.post(
             "/account/verify",
             json.dumps(
                 {
