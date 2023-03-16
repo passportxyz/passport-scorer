bb076287f571faa7d82f36619d2b934da02005d6
schultztimothy <schultz.timothy52@gmail.com>
2023-03-16 13:30:35 -0600

    chore(interface): use modal template

--- interface/components/ApiKeyModal.tsx
+++ interface/components/ApiKeyModal.tsx
@@ -42,64 +42,56 @@ export function ApiKeyCreateModal({
   };
 
   return (
-    <Modal
+    <ModalTemplate
       isOpen={isOpen}
-      isCentered={true}
-      size={{ base: "full", md: "xl", lg: "xl", xl: "xl" }}
       onClose={closeAndReset}
+      footer={() => (
+        <button
+          className="mb-6 mt-auto w-full rounded bg-purple-gitcoinpurple py-3 text-white md:mt-8"
+          onClick={createApiKey}
+          disabled={keyName.length === 0 || inProgress}
+        >
+          Create
+        </button>
+      )}
     >
-      <ModalOverlay />
-      <ModalContent>
-        <ModalCloseButton onClick={closeAndReset} />
-        <ModalBody className="mt-8 flex h-screen w-full flex-col">
-          <div className="w-100 flex flex-col items-center">
-            <div className="w-fit rounded-full bg-[#F0EBFF] p-3 text-purple-gitcoinpurple">
-              <div className="flex w-6 justify-around">
-                <KeyIcon />
-              </div>
-            </div>
+      <div className="w-100 flex flex-col items-center">
+        <div className="w-fit rounded-full bg-[#F0EBFF] p-3 text-purple-gitcoinpurple">
+          <div className="flex w-6 justify-around">
+            <KeyIcon />
           </div>
-          <div className="mt-6 mb-6 text-center">
-            <Text className="text-purple-darkpurple">Generate API Key</Text>
-            <Text className="mt-2 text-center text-purple-softpurple">
-              Name your API key to help identify it in the future.
-            </Text>
-          </div>
-          <div className="flex flex-col">
-            <label className="mb-2 font-librefranklin text-xs text-purple-darkpurple">
-              Key Name
-            </label>
-            <Input
-              className="mb-6"
-              data-testid="key-name-input"
-              value={keyName}
-              onChange={(e) => setKeyName(e.target.value)}
-              placeholder={"Enter the key's name/identifier"}
-              // chakra can't find purple-gitcoinpurple from tailwind :(
-              focusBorderColor="#6f3ff5"
-            />
+        </div>
+      </div>
+      <div className="mt-6 mb-6 text-center">
+        <Text className="text-purple-darkpurple">Generate API Key</Text>
+        <Text className="mt-2 text-center text-purple-softpurple">
+          Name your API key to help identify it in the future.
+        </Text>
+      </div>
+      <div className="flex flex-col">
+        <label className="mb-2 font-librefranklin text-xs text-purple-darkpurple">
+          Key Name
+        </label>
+        <Input
+          className="mb-6"
+          data-testid="key-name-input"
+          value={keyName}
+          onChange={(e) => setKeyName(e.target.value)}
+          placeholder={"Enter the key's name/identifier"}
+          // chakra can't find purple-gitcoinpurple from tailwind :(
+          focusBorderColor="#6f3ff5"
+        />
 
-            <p className="mb-1 text-xs italic text-purple-softpurple">
-              i.e. &#39;Gitcoin dApp - Prod&#39;, or &#39;Snapshot discord
-              bot&#39;, or &#39;Bankless Academy testing&#39;, etc.
-            </p>
-            <hr />
-            {creationError.length > 0 && (
-              <p className="pt-4 text-red-700">{creationError}</p>
-            )}
-          </div>
-        </ModalBody>
-        <ModalFooter>
-          <button
-            className="mb-6 mt-auto w-full rounded bg-purple-gitcoinpurple py-3 text-white md:mt-8"
-            onClick={createApiKey}
-            disabled={keyName.length === 0 || inProgress}
-          >
-            Create
-          </button>
-        </ModalFooter>
-      </ModalContent>
-    </Modal>
+        <p className="mb-1 text-xs italic text-purple-softpurple">
+          i.e. &#39;Gitcoin dApp - Prod&#39;, or &#39;Snapshot discord bot&#39;,
+          or &#39;Bankless Academy testing&#39;, etc.
+        </p>
+        <hr />
+        {creationError.length > 0 && (
+          <p className="pt-4 text-red-700">{creationError}</p>
+        )}
+      </div>
+    </ModalTemplate>
   );
 }
 
@@ -133,47 +125,45 @@ export function ApiKeyUpdateModal({
   };
 
   return (
-    <>
-      <ModalTemplate
-        isOpen={isOpen}
-        onClose={closeAndReset}
-        header={() => (
-          <span className="text-base font-normal">Rename API Key</span>
-        )}
-        footer={() => (
-          <button
-            className="mb-6 mt-auto w-full rounded bg-purple-gitcoinpurple py-3 text-white md:mt-8"
-            onClick={updateApiKey}
-            disabled={name.length === 0 || inProgress}
-          >
-            Save Changes
-          </button>
-        )}
-      >
-        <div className="flex flex-col">
-          <label className="mb-2 font-librefranklin text-xs text-purple-darkpurple">
-            Key Name
-          </label>
-          <Input
-            className="mb-6"
-            data-testid="key-name-input"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-            placeholder={"Enter the key's name/identifier"}
-            // chakra can't find purple-gitcoinpurple from tailwind :(
-            focusBorderColor="#6f3ff5"
-          />
+    <ModalTemplate
+      isOpen={isOpen}
+      onClose={closeAndReset}
+      header={() => (
+        <span className="text-base font-normal">Rename API Key</span>
+      )}
+      footer={() => (
+        <button
+          className="mb-6 mt-auto w-full rounded bg-purple-gitcoinpurple py-3 text-white md:mt-8"
+          onClick={updateApiKey}
+          disabled={name.length === 0 || inProgress}
+        >
+          Save Changes
+        </button>
+      )}
+    >
+      <div className="flex flex-col">
+        <label className="mb-2 font-librefranklin text-xs text-purple-darkpurple">
+          Key Name
+        </label>
+        <Input
+          className="mb-6"
+          data-testid="key-name-input"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          placeholder={"Enter the key's name/identifier"}
+          // chakra can't find purple-gitcoinpurple from tailwind :(
+          focusBorderColor="#6f3ff5"
+        />
 
-          <p className="mb-1 text-xs italic text-purple-softpurple">
-            i.e. &#39;Gitcoin dApp - Prod&#39;, or &#39;Snapshot discord
-            bot&#39;, or &#39;Bankless Academy testing&#39;, etc.
-          </p>
-          <hr />
-          {updateError.length > 0 && (
-            <p className="pt-4 text-red-700">{updateError}</p>
-          )}
-        </div>
-      </ModalTemplate>
-    </>
+        <p className="mb-1 text-xs italic text-purple-softpurple">
+          i.e. &#39;Gitcoin dApp - Prod&#39;, or &#39;Snapshot discord bot&#39;,
+          or &#39;Bankless Academy testing&#39;, etc.
+        </p>
+        <hr />
+        {updateError.length > 0 && (
+          <p className="pt-4 text-red-700">{updateError}</p>
+        )}
+      </div>
+    </ModalTemplate>
   );
 }
