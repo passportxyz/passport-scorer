125c319d28d1dddbd3fbcb02d9dc3c478dc4815f
nutrina <nutrina9@gmail.com>
2023-03-22 19:36:20 +0200

    test(ui,dashboard): un-skipping test for dashboard component

--- interface/__tests__/components/Dashboard.test.tsx
+++ interface/__tests__/components/Dashboard.test.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import { fireEvent, render, screen, waitFor } from "@testing-library/react";
 import Dashboard from "../../components/Dashboard";
+import PageLayout, { TopLevelPageContext } from "../../components/PageLayout";
+import { MemoryRouter, Routes, Route } from "react-router-dom";
+
 import {
   renderWithContext,
   makeTestUserContext,
@@ -17,22 +20,40 @@ jest.mock("../../components/Header", () => {
 
 const mockUserContext: UserState = makeTestUserContext();
 
+const AppCustomHeader = (): JSX.Element => <div>My custom header</div>;
+const AppCustomFooter = (): JSX.Element => <div>My custom footer</div>;
+
 describe("Dashboard", () => {
-  it.skip("should render the header", async () => {
+  it("should render the header", async () => {
+    const context: TopLevelPageContext = {
+      generateHeader: (Subheader?: React.ComponentType) => {
+        return AppCustomHeader;
+      },
+      generateFooter: (FooterOverride?: React.ComponentType) => {
+        return AppCustomFooter;
+      },
+    };
     renderWithContext(
       mockUserContext,
-      <Dashboard activeTab="api-keys" authenticationStatus="authenticated">
-        <div></div>
-      </Dashboard>
+      <MemoryRouter initialEntries={["/dashboard/api-keys"]}>
+        <Routes>
+          <Route element={<PageLayout />}>
+            <Route path="/dashboard" element={<Dashboard />}>
+              <Route path="/dashboard/scorer" element={<div />} />
+              <Route path="/dashboard/api-keys" element={<div />} />
+            </Route>
+          </Route>
+        </Routes>
+      </MemoryRouter>
     );
     const activeTab = screen.getByTestId("api-keys-tab");
     const notActiveTab = screen.getByTestId("scorer-tab");
     await waitFor(async () => {
       expect(activeTab).toHaveClass(
-        "flex w-full items-center justify-start rounded-sm px-3 py-2 text-blue-darkblue rounded border border-gray-lightgray bg-white"
+        "flex w-full items-center justify-start rounded-md px-3 py-2 text-blue-darkblue border border-gray-lightgray bg-white"
       );
       expect(notActiveTab).toHaveClass(
-        "flex w-full items-center justify-start rounded-sm px-3 py-2 text-blue-darkblue mb-2"
+        "flex w-full items-center justify-start rounded-md px-3 py-2 text-blue-darkblue mb-2"
       );
     });
   });
