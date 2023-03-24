a1122cc17d44eb8a362b4cbdefbb8a7ce00c1edc
nutrina <nutrina9@gmail.com>
2023-03-24 11:21:23 +0200

    feat(test): improving tests for authentication, verify also correctly submitted token and returned HTTP error

--- api/registry/test/test_api_auth_required.py
+++ api/registry/test/test_api_auth_required.py
@@ -42,6 +42,36 @@ def test_authentication_is_required_token(api_path_that_requires_auth):
     assert response.status_code == 401
 
 
+def test_authentication_works_with_token(api_path_that_requires_auth, scorer_user):
+    """
+    Test that API key is accepted if it is valid token and present in the HTTP_AUTHORIZATION header
+    """
+    method, path = api_path_that_requires_auth
+    client = Client()
+
+    web3_account = web3.eth.account.from_mnemonic(
+        my_mnemonic, account_path="m/44'/60'/0'/0/0"
+    )
+
+    account = Account.objects.create(user=scorer_user, address=web3_account.address)
+
+    (_, secret) = AccountAPIKey.objects.create_key(
+        account=account, name="Token for user 1"
+    )
+
+    method_fn = client.get
+    if method == "post":
+        method_fn = client.post
+
+    response = method_fn(
+        path,
+        HTTP_AUTHORIZATION="Token " + secret,
+    )
+    # We should not get back any unauuthorized or forbidden errors
+    assert response.status_code != 401
+    assert response.status_code != 403
+
+
 def test_authentication_is_required_api_key(api_path_that_requires_auth):
     """
     Test that bad api keys are rejected
@@ -84,4 +114,6 @@ def test_authentication_works_with_api_key(api_path_that_requires_auth, scorer_u
         path,
         **{"HTTP_X-API-Key": secret},
     )
+    # We should not get back any unauuthorized or forbidden errors
     assert response.status_code != 401
+    assert response.status_code != 403
