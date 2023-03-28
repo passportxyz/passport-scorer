927270293835213d94cc1283eaa496b621400b5d
Aminah Burch <hello@aminah.io>
2023-03-28 22:43:27 +0300

    fix(interface): adds warning toast

--- interface/components/Toasts.tsx
+++ interface/components/Toasts.tsx
@@ -1,4 +1,5 @@
 import { CheckCircleIcon, CloseIcon } from "@chakra-ui/icons";
+import { ExclamationCircleIcon } from "@heroicons/react/24/outline";
 import { useToast, UseToastOptions } from "@chakra-ui/react";
 
 export const successToast = (
@@ -35,4 +36,42 @@ export const successToast = (
       </div>
     ),
   };
+};
+
+export const warningToast = (
+  message: string,
+  toast: ReturnType<typeof useToast>
+): UseToastOptions => {
+  return {
+    title: "Warning!",
+    status: "warning",
+    duration: null,
+    isClosable: true,
+    variant: "solid",
+    position: "bottom",
+    render: () => (
+      <div
+        style={{
+          backgroundColor: "#FDDEE4",
+          borderRadius: "4px",
+          display: "flex",
+          alignItems: "center",
+          padding: "16px",
+          marginBottom: "80px"
+        }}
+      >
+        <ExclamationCircleIcon className="mr-3 w-6 text-[#D44D6E]" />
+        <span style={{ color: "#0E0333", fontSize: "16px" }}>
+          {message}
+        </span>
+        <CloseIcon
+          color="#0E0333"
+          boxSize={3}
+          ml="8"
+          cursor="pointer"
+          onClick={() => toast.closeAll()}
+        />
+      </div>
+    ),
+  }
 };
