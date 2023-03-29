172d8e8053413aaeb0ee9d88f3584de91cf9815b
schultztimothy <schultz.timothy52@gmail.com>
2023-03-29 16:51:40 -0600

    feat(api): add request_analytics middleware

--- api/scorer/settings/base.py
+++ api/scorer/settings/base.py
@@ -86,6 +86,7 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
     "social_django.middleware.SocialAuthExceptionMiddleware",
+    "scorer.middleware.request_analytics.ApiKeyRequestCounterMiddleware",
 ]
 
 ROOT_URLCONF = "scorer.urls"
