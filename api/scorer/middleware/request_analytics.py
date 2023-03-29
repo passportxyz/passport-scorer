546c9eaccefbb9ea1925dff371f29ea3251a5946
schultztimothy <schultz.timothy52@gmail.com>
2023-03-29 16:51:40 -0600

    feat(api): add request_analytics middleware

---
+++ api/scorer/middleware/request_analytics.py
@@ -0,0 +1,31 @@
+import logging
+
+from registry.tasks import save_api_key_analytics
+
+log = logging.getLogger(__name__)
+
+
+class ApiKeyRequestCounterMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        response = self.get_response(request)
+
+        try:
+            if request.path.startswith("/registry/"):
+                # Extract the API key from the request header
+                api_key_value = request.META.get("HTTP_X_API_KEY")
+
+                if api_key_value:
+                    # Using a task here to avoid blocking the request
+                    save_api_key_analytics.delay(api_key_value, request.path)
+
+        except Exception as e:
+            log.error(
+                "Exception while saving api key analytics. exception='%s'",
+                e,
+            )
+            pass
+
+        return response
