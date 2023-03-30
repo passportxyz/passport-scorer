70d846982a7c1d95cd690e6da69b24235c0c1069
Aminah Burch <hello@aminah.io>
2023-03-30 18:15:23 +0300

    fix(interface): adds reusable warning toast

--- interface/components/Toasts.tsx
+++ interface/components/Toasts.tsx
@@ -36,3 +36,41 @@ export const successToast = (
     ),
   };
 };
+
+export const warningToast = (
+  message: string,
+  toast: ReturnType<typeof useToast>
+): UseToastOptions => {
+  return {
+    title: "Warning!",
+    status: "warning",
+    duration: null,
+    isClosable: true,
+    variant: "solid",
+    position: "bottom",
+    render: () => (
+      <div
+        style={{
+          backgroundColor: "#FDDEE4",
+          borderRadius: "4px",
+          display: "flex",
+          alignItems: "center",
+          padding: "16px",
+          marginBottom: "80px"
+        }}
+      >
+        <ExclamationCircleIcon className="mr-3 w-6 text-[#D44D6E]" />
+        <span style={{ color: "#0E0333", fontSize: "16px" }}>
+          {message}
+        </span>
+        <CloseIcon
+          color="#0E0333"
+          boxSize={3}
+          ml="8"
+          cursor="pointer"
+          onClick={() => toast.closeAll()}
+        />
+      </div>
+    ),
+  }
+};
