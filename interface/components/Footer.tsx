4da1b48ed0e57c817a3f44856e8ac1b6e6861b31
Chibuotu Amadi <chibuotu@gmail.com>
2023-03-31 12:34:12 +0100

    feat: add git commit hash
    Co-authored-by: Gerald Iakobinyi-Pich <nutrina9@gmail.com>

--- interface/components/Footer.tsx
+++ interface/components/Footer.tsx
@@ -35,15 +35,23 @@ const Footer = ({ mode, className }: FooterProps): JSX.Element => {
           rel="noopener noreferrer"
           className={`text-${assets.emphasisColor} ml-1 hover:underline`}
         >
-          Ceramic.
+          <span className="text-purple-darkpurple">Ceramic</span>
         </a>
       </div>
       <div className="flex">
         <a
+          href={`https://github.com/gitcoinco/passport-scorer/commit/${process.env.NEXT_PUBLIC_GIT_COMMIT_HASH}`}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="mr-8 text-purple-darkpurple"
+        >
+          Git commit
+        </a>
+        <a
           href="https://github.com/gitcoinco/passport"
           target="_blank"
           rel="noopener noreferrer"
-          className="mr-6"
+          className="mr-8"
         >
           <img src={assets.githubLogo} alt="Github Logo" />
         </a>
