1f1f59a518edfa13afe0414a29c2afee24170d95
Michael <michaelgreen06@gmail.com>
2023-04-11 16:52:25 -0500

    fix: add intercom code to app.tsx & delete _document.tsx

--- interface/pages/_app.tsx
+++ interface/pages/_app.tsx
@@ -15,6 +15,52 @@ import ManageAccountCenter from "../components/ManageAccountCenter";
 
 import { UserProvider } from "../context/userContext";
 
+const INTERCOM_APP_ID = process.env.NEXT_PUBLIC_INTERCOM_APP_ID || "";
+
+useEffect(() => {
+  if (typeof window !== "undefined") {
+    // @ts-ignore
+    window.intercomSettings = {
+      api_base: "https://api-iam.intercom.io",
+      app_id: INTERCOM_APP_ID,
+    };
+    //@ts-ignore eslint-disable-next-line no-use-before-define
+    (function () {
+      var w: any = window;
+      var ic = w.Intercom;
+      if (typeof ic === "function") {
+        ic("reattach_activator");
+        ic("update", w.intercomSettings);
+      } else {
+        var d = document;
+        var i = function () {
+          i.c(arguments);
+        };
+        i.q = [];
+        i.c = function (args) {
+          i.q.push(args);
+        };
+        w.Intercom = i;
+        var l = function () {
+          var s = d.createElement("script");
+          s.type = "text/javascript";
+          s.async = true;
+          s.src = "https://widget.intercom.io/widget/xaafeyri";
+          var x = d.getElementsByTagName("script")[0];
+          x.parentNode.insertBefore(s, x);
+        };
+        if (document.readyState === "complete") {
+          l();
+        } else if (w.attachEvent) {
+          w.attachEvent("onload", l);
+        } else {
+          w.addEventListener("load", l, false);
+        }
+      }
+    })();
+  }
+}, []);
+
 export default function App({ Component, pageProps }: AppProps) {
   //  Following guide from https://colinhacks.com/essays/building-a-spa-with-nextjs to turn nextjs app into spa
   const [isServer, setIsServer] = useState(true);
