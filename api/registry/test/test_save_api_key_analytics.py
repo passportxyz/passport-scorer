45cbd8e8a9ffba4bb6778b1c4573a625319066aa
schultztimothy <schultz.timothy52@gmail.com>
2023-04-24 13:22:04 -0600

    fix(api): remove unneeded test

--- api/registry/test/test_save_api_key_analytics.py
+++ api/registry/test/test_save_api_key_analytics.py
@@ -1,5 +1,4 @@
 from datetime import datetime
-from unittest.mock import MagicMock, patch
 
 import pytest
 from account.models import AccountAPIKey, AccountAPIKeyAnalytics
@@ -23,13 +22,3 @@ class TestSaveApiKeyAnalytics:
 
         assert created_at_day.day is datetime.now().day
         assert created_at_day.month is datetime.now().month
-
-    def test_invalid_secret(self, scorer_account):
-        (model, secret) = AccountAPIKey.objects.create_key(
-            account=scorer_account, name="Another token for user 1"
-        )
-
-        save_api_key_analytics(2, path)
-
-        obj = AccountAPIKeyAnalytics.objects.filter(path=path, api_key=model)
-        assert obj.count() is 0
