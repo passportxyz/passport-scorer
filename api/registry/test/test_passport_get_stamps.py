956c215b61eff1f30d203ebd27f18060178ec8ae
Lucian <lucian@gitcoin.co>
2023-05-08 13:18:50 -0700

    feat(api): read stamps from cache for registry api endpoint (#239)
    * feat(api): read stamps from cache for registry api endpoint

--- api/registry/test/test_passport_get_stamps.py
+++ api/registry/test/test_passport_get_stamps.py
@@ -1,8 +1,8 @@
 import pytest
+from ceramic_cache.models import CeramicCache
 from django.conf import settings
 from django.contrib.auth import get_user_model
 from django.test import Client
-from registry.models import Passport, Stamp
 from web3 import Web3
 
 User = get_user_model()
@@ -15,19 +15,16 @@ pytestmark = pytest.mark.django_db
 
 @pytest.fixture
 def paginated_stamps(scorer_community, passport_holder_addresses):
-    passport = Passport.objects.create(
-        address=passport_holder_addresses[0]["address"],
-        community=scorer_community,
-    )
+    address = passport_holder_addresses[0]["address"]
 
     stamps = []
 
     for i in range(10):
-        stamp = Stamp.objects.create(
-            passport=passport,
-            hash=f"v0.0.0:Ft7mqRdvJ9jNgSSowb9qdcMeOzswOeighIOvk0wn96{i}=",
-            provider=f"Provider{i}",
-            credential={
+        provider = f"Provider{i}"
+        cacheStamp = CeramicCache.objects.create(
+            address=address,
+            provider=provider,
+            stamp={
                 "type": ["VerifiableCredential"],
                 "proof": {
                     "jws": "eyJhbGciOiJFZERTQSIsImNyaXQiOlsiYjY0Il0sImI2NCI6ZmFsc2V9..34uD8jKn2N_yE8pY4ErzVD8pJruZq7qJaCxx8y0SReY2liZJatfeQUv1nqmZH19a-svOyfHt_VbmKvh6A5vwBw",
@@ -49,11 +46,11 @@ def paginated_stamps(scorer_community, passport_holder_addresses):
                             "provider": "https://schema.org/Text",
                         }
                     ],
-                    "provider": f"Provider{i}",
+                    "provider": provider,
                 },
             },
         )
-        stamps.append(stamp)
+        stamps.append(cacheStamp)
 
     return stamps
 
@@ -71,6 +68,37 @@ class TestPassportGetStamps:
 
         assert response.status_code == 200
         assert len(response_data["items"]) == 0
+
+    def test_get_stamps_only_includes_this_address(
+        self,
+        scorer_api_key,
+        passport_holder_addresses,
+        paginated_stamps,
+    ):
+        paginated_stamps.reverse()
+        last_stamp = paginated_stamps[-1]
+
+        # Add another stamp with a different address
+        CeramicCache.objects.create(
+            address=passport_holder_addresses[1]["address"],
+            provider=last_stamp.provider,
+            stamp=last_stamp.stamp,
+        )
+
+        limit = 20
+
+        client = Client()
+        response = client.get(
+            f"/registry/stamps/{passport_holder_addresses[0]['address']}?limit={limit}",
+            HTTP_AUTHORIZATION="Token " + scorer_api_key,
+        )
+
+        assert response.status_code == 200
+
+        response_data = response.json()
+        assert len(response_data["items"]) == len(paginated_stamps)
+
+        assert len(CeramicCache.objects.all()) == len(paginated_stamps) + 1
 
     def test_get_stamps_returns_first_page_stamps(
         self,
