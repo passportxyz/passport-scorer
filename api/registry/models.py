490ec293e35eb7d79fc3694ba7dce991ec77fe07
Gerald Iakobinyi-Pich <nutrina9@gmail.com>
2023-06-01 14:08:13 +0300

    fix(api): improving the serialisation of some of the models, to make debugging easier

--- api/registry/models.py
+++ api/registry/models.py
@@ -12,7 +12,7 @@ class Passport(models.Model):
         unique_together = ["address", "community"]
 
     def __str__(self):
-        return f"{self.address}"
+        return f"Passport #{self.id} for address {self.address} in community [{self.community}]"
 
 
 class Stamp(models.Model):
@@ -26,7 +26,7 @@ class Stamp(models.Model):
     credential = models.JSONField(default=dict)
 
     def __str__(self):
-        return f"#{self.hash}"
+        return f"Stamp #{self.id} for passport [{self.passport}] hash #{self.hash} provider {self.provider}"
 
     class Meta:
         unique_together = ["hash", "passport"]
@@ -54,3 +54,6 @@ class Score(models.Model):
     )
     error = models.TextField(null=True, blank=True)
     evidence = models.JSONField(null=True, blank=True)
+
+    def __str__(self):
+        return f"Score #{self.id} for passport [{self.passport}]: score={self.score}, status={self.status}, last_score_timestamp={self.last_score_timestamp}, error={self.error}, evidence={self.evidence}"
