2a01a0b74b3bdbc6351339c0c9c88d73efed104a
schultztimothy <schultz.timothy52@gmail.com>
2023-08-03 08:56:54 -0600

    chore(api): unit tests for admin endpoints

--- api/passport_admin/tests.py
+++
@@ -1,5 +0,0 @@
-# from django.test import TestCase
-
-"""
-Tests for Passport Admin
-"""
---
+++ api/passport_admin/tests/test_passport_admin.py
@@ -0,0 +1,54 @@
+import copy
+import json
+from collections import namedtuple
+
+import pytest
+from account.models import Nonce
+from ceramic_cache.api import DbCacheToken
+from django.test import Client
+from ninja_jwt.tokens import AccessToken
+from passport_admin.api import get_address
+from passport_admin.models import DismissedBanners, PassportBanner
+
+pytestmark = pytest.mark.django_db
+
+client = Client()
+
+
+class TestPassPortAdmin:
+    def test_get_address(self):
+        assert (
+            get_address("did:pkh:eip155:1:0xc79abb54e4824cdb65c71f2eeb2d7f2db5da1fb8")
+            == "0xc79abb54e4824cdb65c71f2eeb2d7f2db5da1fb8"
+        )
+
+    def test_get_banners(self, sample_token):
+        response = client.get(
+            "/passport-admin/banners",
+            HTTP_AUTHORIZATION=f"Bearer {sample_token}",
+            content_type="application/json",
+        )
+        assert response.json() == []
+
+    def test_dismiss_banner(self, sample_token, sample_address):
+        banner = PassportBanner.objects.create(name="test", content="test", link="test")
+        response = client.post(
+            f"/passport-admin/banners/{banner.pk}/dismiss",
+            {},
+            content_type="application/json",
+            HTTP_AUTHORIZATION=f"Bearer {sample_token}",
+        )
+        assert response.json() == {"status": "success"}
+        dismissed_banner = DismissedBanners.objects.get(
+            banner=banner, address=sample_address.lower()
+        )
+        assert dismissed_banner.banner == banner
+
+    def test_dismiss_banner_does_not_exist(self, sample_token):
+        response = client.post(
+            "/passport-admin/banners/1/dismiss",
+            {},
+            content_type="application/json",
+            HTTP_AUTHORIZATION=f"Bearer {sample_token}",
+        )  # Needs JWT Auth
+        assert response.json() == {"status": "failed"}
