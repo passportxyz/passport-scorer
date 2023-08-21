2395361e8031a03e991aefdcabd3b6b6a721d505
Lucian Hymer <lucian.hymer@gmail.com>
2023-08-21 15:17:29 -0700

    fix(infra): added updated_at to CeramicCache, modified dump script to use this

--- api/ceramic_cache/management/commands/dump_stamp_data.py
+++ api/ceramic_cache/management/commands/dump_stamp_data.py
@@ -8,6 +8,7 @@ from django.conf import settings
 from django.core.management.base import BaseCommand
 from django.core.paginator import Paginator
 from django.utils import timezone
+from tqdm import tqdm
 
 s3 = boto3.client(
     "s3",
@@ -26,25 +27,58 @@ class Command(BaseCommand):
 
         if not latest_export:
             print("No previous exports found. Exporting all data.")
-            latest_export = StampExports.objects.create(
-                last_export_ts=timezone.now() - datetime.timedelta(days=7)
+            latest_export = StampExports(
+                last_export_ts=datetime.date.fromisoformat("1970-01-01")
             )
 
-        paginator = Paginator(
-            CeramicCache.objects.filter(
-                created_at__gt=latest_export.last_export_ts
-            ).values_list("stamp", flat=True),
-            1000,
+        print(f"Getting Stamps updated since {latest_export.last_export_ts}")
+
+        query = (
+            CeramicCache.objects.filter(created_at__gt=latest_export.last_export_ts)
+            .values("stamp", "updated_at")
+            .order_by("updated_at")
+            .using("read_replica_0")
         )
 
         # Generate the dump file name
         file_name = f'stamps_{latest_export.last_export_ts.strftime("%Y%m%d_%H%M%S")}_{timezone.now().strftime("%Y%m%d_%H%M%S")}.jsonl'
 
-        # Write serialized data to the file
-        with open(file_name, "w") as f:
-            for page in paginator.page_range:
-                for stamp in paginator.page(page).object_list:
-                    f.write(json.dumps({"stamp": stamp}) + "\n")
+        last_updated_at = latest_export.last_export_ts
+        chunk_size = 1000
+
+        try:
+            # Write serialized data to the file
+            with open(file_name, "w") as f:
+                with tqdm(
+                    unit="items", unit_scale=None, desc="Exporting stamps"
+                ) as progress_bar:
+                    has_more = True
+                    while has_more:
+                        objects = list(
+                            query.filter(updated_at__gt=last_updated_at)[:chunk_size]
+                        )
+                        if objects:
+                            num_objects = len(objects)
+                            progress_bar.update(num_objects)
+
+                            for cache_obj in objects:
+                                f.write(
+                                    json.dumps({"stamp": cache_obj["stamp"]}) + "\n"
+                                )
+
+                            last_updated_at = cache_obj["updated_at"]
+
+                            # If we get less than the chunk size, we've reached the end
+                            # No need to keep querying which could result in querying forever
+                            if num_objects < chunk_size:
+                                has_more = False
+                        else:
+                            has_more = False
+
+        finally:
+            self.stdout.write(
+                self.style.SUCCESS(f'Last stamp updated at "{last_updated_at}"')
+            )
 
         # Upload to S3 bucket
         s3.upload_file(file_name, settings.S3_WEEKLY_BACKUP_BUCKET_NAME, file_name)
@@ -53,7 +87,7 @@ class Command(BaseCommand):
         os.remove(file_name)
 
         StampExports.objects.create(
-            last_export_ts=timezone.now(), stamp_total=paginator.count
+            last_export_ts=last_updated_at, stamp_total=progress_bar.n
         )
 
         print(f"Data dump completed and uploaded to S3 as {file_name}")
