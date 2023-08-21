85041e15b3444a8e3281ede290713e64a209e579
Gerald Iakobinyi-Pich <nutrina9@gmail.com>
2023-08-21 14:53:30 +0300

    fix(infra):  enable performance insights for DBs

--- infra/prod/index.ts
+++ infra/prod/index.ts
@@ -90,6 +90,7 @@ const postgresql = new aws.rds.Instance(
     vpcSecurityGroupIds: [db_secgrp.id],
     deletionProtection: true,
     backupRetentionPeriod: 5,
+    performanceInsightsEnabled: true,
   },
   { protect: true }
 );
@@ -105,6 +106,7 @@ const readreplica0 = new aws.rds.Instance(
     deletionProtection: true,
     backupRetentionPeriod: 5,
     replicateSourceDb: postgresql.id,
+    performanceInsightsEnabled: true,
   },
   { protect: true }
 );
