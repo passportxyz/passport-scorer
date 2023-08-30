f0ef73fa325623b034c7b2a6f96cf134046dbfed
Aminah <58310685+aminah-io@users.noreply.github.com>
2023-08-30 16:08:08 +0300

    feat(api): updates django admin views to use ScorerModelAdmin class which removes pagination (#375)

--- api/scorer_apu/admin.py
+++ api/scorer_apu/admin.py
@@ -1,24 +1,25 @@
 from django.contrib import admin
+from scorer.scorer_admin import ScorerModelAdmin
 from scorer_apu.models import ApuScorer, Combo, NumInfo
 
 # Register your models here.
 
 
-class ApuScorerAdmin(admin.ModelAdmin):
-    list_display = ["start_time", "end_time", "accepted_providers"]
-
-
-class ScoreAdmin(admin.ModelAdmin):
+class ScoreAdmin(ScorerModelAdmin):
     list_display = ["passport", "scorer"]
     search_fields = ["passport__did"]
 
 
-class ComboAdmin(admin.ModelAdmin):
+class ApuScorerAdmin(ScorerModelAdmin):
+    list_display = ["start_time", "end_time", "accepted_providers"]
+
+
+class ComboAdmin(ScorerModelAdmin):
     list_display = ["scorer", "passport", "combo", "count"]
     search_fields = ["passport__did"]
 
 
-class NumInfoAdmin(admin.ModelAdmin):
+class NumInfoAdmin(ScorerModelAdmin):
     list_display = ["scorer", "stamp_count", "count"]
 
 
