b9bc004b1886e9ae925f579547274848196805a0
schultztimothy <schultz.timothy52@gmail.com>
2023-09-26 17:53:51 -0600

    feat(indexer): initial indexer setup

---
+++ indexer/Readme.md
@@ -0,0 +1,8 @@
+# Database management
+
+- Using Prisma ORM to manage inserts into scorer-db
+- Migrations
+
+```bash
+
+```
---
+++ indexer/index.ts
@@ -0,0 +1,28 @@
+import ethers from "ethers";
+import { createIndexer, Event, Indexer } from "chainsauce";
+import { PostgresStorage } from "./storage/postgres";
+
+import IDStakingABI from "./abis/IDStaking.json" assert { type: "json" };
+
+async function handleEvent(indexer: Indexer<PostgresStorage>, event: Event) {
+  const db = indexer.storage;
+
+  switch (event.name) {
+    case "UserCreated":
+      // db.collection("users").insert({
+      //   id: event.args.id,
+      //   name: event.args.name
+      // });
+      break;
+
+    case "UserUpdated":
+      // db.collection("users").updateById(event.args.id, {
+      //   name: event.args.name
+      // });
+      break;
+  }
+}
+
+const provider = new ethers.providers.JsonRpcProvider("http://mynode.com");
+const storage = new PostgresStorage("./data");
+const indexer = await createIndexer(provider, storage, handleEvent);
