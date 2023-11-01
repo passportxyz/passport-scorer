66f809265be55f5b276b52602d36c7e4935885a5
Aminah Burch <hello@aminah.io>
2023-11-01 18:46:07 +0300

    feat(api): updates filters and tests with readable gtc amounts

--- api/registry/filters.py
+++ api/registry/filters.py
@@ -17,21 +17,21 @@ class GTCStakeEventsFilter(django_filters.FilterSet):
             round_id=round_id_value,
             event_type="Xstake",
             staker=value,
-            amount__gte=5000000000000000000,
+            amount__gte=5,
         ) | Q(address=value)
 
         trusted_citizen_staker = Q(
             round_id=round_id_value,
             event_type="Xstake",
             address=value,
-            amount__gte=20000000000000000000,
+            amount__gte=20,
         )
 
         self_stake = Q(
             round_id=round_id_value,
             event_type="SelfStake",
             staker=value,
-            amount__gte=5000000000000000000,
+            amount__gte=5,
         )
 
         results = queryset.filter(
