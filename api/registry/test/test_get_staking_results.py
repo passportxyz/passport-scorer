f8dc8005d298cfc68681880830f1b444a08b02d4
Aminah Burch <hello@aminah.io>
2023-11-07 21:26:38 +0300

    feat(api): fixes test

--- api/registry/test/test_get_staking_results.py
+++ api/registry/test/test_get_staking_results.py
@@ -7,52 +7,8 @@ pytestmark = pytest.mark.django_db
 user_address = "0x00Ac00000e4AbE2d293586A1f4F9C73e5512121e"
 
 
-@pytest.fixture
-def gtc_staking_response():
-    mock_response = [
-        [
-            {
-                "id": 1,
-                "event_type": "SelfStake",
-                "round_id": 1,
-                "staker": "0x",
-                "address": None,
-                "amount": "11000000000000000000",
-                "staked": True,
-                "block_number": 16,
-                "tx_hash": "0x93",
-            },
-        ],
-        [
-            {
-                "id": 2,
-                "event_type": "Xstake",
-                "round_id": 1,
-                "staker": "0x09",
-                "address": "0x",
-                "amount": "10000000000000000000",
-                "staked": True,
-                "block_number": 16,
-                "tx_hash": "0xd08",
-            },
-            {
-                "id": 3,
-                "event_type": "Xstake",
-                "round_id": 1,
-                "staker": "0x",
-                "address": "0x6",
-                "amount": "11000000000000000000",
-                "staked": True,
-                "block_number": 16,
-                "tx_hash": "0x94",
-            },
-        ],
-    ]
-
-    return mock_response
-
-
 class TestGetStakingResults:
+    @pytest.fixture
     def test_successful_get_staking_results(self, scorer_api_key, gtc_staking_response):
         stakes = list(GTCStakeEvent.objects.all())
 
@@ -68,6 +24,7 @@ class TestGetStakingResults:
         # an extra stake event was added that is below the filtered amount, hence the minus 1
         assert len(stakes) - 1 == len(response_data)
 
+    @pytest.fixture
     def test_item_in_filter_condition_is_not_present(
         self, scorer_api_key, gtc_staking_response
     ):
@@ -84,55 +41,17 @@ class TestGetStakingResults:
             # ID 16 belongs to the item that does not meet the filter criteria
             assert item["id"] != 16
 
+    @pytest.fixture
     def test_missing_address_error_response(self, scorer_api_key, gtc_staking_response):
         client = Client()
         response = client.get(
-            f"/registry/gtc-stake//1",
+            "/registry/gtc-stake//1",
             HTTP_AUTHORIZATION="Token " + scorer_api_key,
         )
 
         assert response.status_code == 404
 
-    def test_missing_round_id_error_response(
-        self, scorer_api_key, gtc_staking_response
-    ):
-        client = Client()
-        response = client.get(
-            f"/registry/gtc-stake/{user_address}/",
-            HTTP_AUTHORIZATION="Token " + scorer_api_key,
-        )
-
-        response_data = response.json()["results"]
-        assert response.status_code == 200
-
-        # an extra stake event was added that is below the filtered amount, hence the minus 1
-        assert len(stakes) - 1 == len(response_data)
-
-    def test_item_in_filter_condition_is_not_present(
-        self, scorer_api_key, gtc_staking_response
-    ):
-        client = Client()
-        response = client.get(
-            f"/registry/gtc-stake/{user_address}/1",
-            HTTP_AUTHORIZATION="Token " + scorer_api_key,
-        )
-
-        response_data = response.json()["results"]
-        assert response.status_code == 200
-
-        for item in response_data:
-            # ID 16 belongs to the item that does not meet the filter criteria
-            assert item["id"] != 16
-
-    def test_missing_address_error_response(self, scorer_api_key, gtc_staking_response):
-        client = Client()
-        response = client.get(
-            f"/registry/gtc-stake//1",
-            HTTP_AUTHORIZATION="Token " + scorer_api_key,
-        )
-
-        assert response.status_code == 404
-
+    @pytest.fixture
     def test_missing_round_id_error_response(
         self, scorer_api_key, gtc_staking_response
     ):
