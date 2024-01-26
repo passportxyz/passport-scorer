d7c2cdd01517ab592a22dad5dcc46916ad904cbe
Gerald Iakobinyi-Pich <nutrina9@gmail.com>
2024-01-26 22:50:36 +0200

    fix[infra]: install docker-compose in redash instances

--- infra/staging/index.ts
+++ infra/staging/index.ts
@@ -1011,6 +1011,11 @@ const redashInitScript = redashDbUrl.apply((url) =>
       redashMailPassword.apply(
         (mailPassword) =>
           `#!/bin/bash
+
+          echo "Install docker-compose ..."
+          sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+          sudo chmod +x /usr/local/bin/docker-compose
+
           echo "Setting environment variables..."
           export POSTGRES_PASSWORD="${dbPassword}"
           export REDASH_DATABASE_URL="${url}"
