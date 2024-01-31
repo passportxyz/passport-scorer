20fef6ad2d31d996502f03283e898c522d215d0c
Gerald Iakobinyi-Pich <nutrina9@gmail.com>
2024-01-31 15:16:19 +0200

    wip

--- api/ceramic_cache/utils.py
+++ api/ceramic_cache/utils.py
@@ -6,7 +6,6 @@ from pprint import pprint
 import api_logging as logging
 import base58
 import dag_cbor
-from multibase import decode
 from multiformats import CID
 from nacl.signing import VerifyKey
 
@@ -59,7 +58,7 @@ def base64url_to_json(base64url_string):
     return json_data
 
 
-def verify_jws(data):
+def verify_jws_old(data):
     """
     https://github.com/decentralized-identity/did-jwt/blob/master/src/JWT.ts
 
@@ -84,3 +83,63 @@ def verify_jws(data):
 
     vk = VerifyKey(decoded_bytes)
     vk.verify(signing_input, signature)
+
+
+def verify_jws(data):
+    p = base64url_to_json(data["signatures"][0]["protected"])
+
+    print("X" * 40)
+    from pprint import pprint
+
+    kid = p["kid"]
+
+    kid_key = kid.split("#")[1]
+    print("XXXXXXXXX")
+    print("kid_key", kid_key)
+
+    pprint(p)
+
+    from Crypto.PublicKey import ECC
+    from jose import jwk, jws
+
+    signature = data["signatures"][0]["signature"]
+    print("protected:", data["signatures"][0]["protected"])
+    print("signature:", type(signature), signature)
+    print("------")
+    signing_input = (
+        data["signatures"][0]["protected"] + "." + data["payload"] + "." + signature
+    )
+    print("signing_input:", signing_input)
+    print("kid          :", kid)
+    print("alg          :", p["alg"])
+    print("------")
+
+    from multibase import decode
+
+    decoded_key = decode(kid_key)
+    print(" decoded_key:", len(decoded_key), decoded_key)
+
+    test_key = jwk.construct(
+        {
+            "kty": "EC",
+            "use": "sig",
+            "crv": "P-256",
+            "kid": "test",
+            "x": "iPNJybBQE5C6EBDEH8Ru-E6NNkBT_ELXD3ACOitKiww",
+            "y": "Qv0E2zjnjJHBbRlLW3hKoEXYQr1FjYX1f2OJKiG7LvE",
+            "alg": "ES256",
+        }
+    )
+
+    # test_key = jwk.construct(decoded_key, "ES256")
+
+    print("     kid_key:", kid_key)
+    print("     byte[0]:", decoded_key[0])
+    print("     byte[1]:", decoded_key[1])
+
+    decoded_bytes = decoded_key[2:]
+
+    eccKey = ECC.EccKey(decoded_bytes)
+    print("eccKey: ", eccKey)
+
+    jws.verify(signing_input, decoded_bytes, algorithms=[p["alg"]])
