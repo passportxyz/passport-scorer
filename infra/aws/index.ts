13792d9866f8899bcd72dfdbc4a8d83934ae353c
Larisa Bucur <larisabucur@192-168-0-147.rdsnet.ro>
2024-02-29 12:59:52 +0200

    remove duplicated ids

--- infra/aws/index.ts
+++ infra/aws/index.ts
@@ -1053,7 +1053,7 @@ export const frequentAlloScorerDataDumpTaskDefinition = createScheduledTask(
 );
 
 export const frequentScorerDataDumpTaskDefinitionForScorer_335 = createScheduledTask(
-  "frequent-allo-scorer-data-dump",
+  "frequent-allo-scorer-data-dump-335",
   {
     ...baseScorerServiceConfig,
     securityGroup: secgrp,
@@ -1082,7 +1082,7 @@ export const frequentScorerDataDumpTaskDefinitionForScorer_335 = createScheduled
 );
 
 export const frequentScorerDataDumpTaskDefinitionForScorer_6608 = createScheduledTask(
-  "frequent-allo-scorer-data-dump",
+  "frequent-allo-scorer-data-dump-6608",
   {
     ...baseScorerServiceConfig,
     securityGroup: secgrp,
