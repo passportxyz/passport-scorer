f3cbfb81cb46c6d8c1d3d665098c0eb35a5e89e7
nutrina <nutrina9@gmail.com>
2024-03-28 22:07:51 +0200

    feat(api): reworking the admin and model for tos. Have introduced a '… (#558)
    * feat(api): reworking the admin and model for tos. Have introduced a 'final' flag for tos. Once a tos is final it cannot be changed any more (from the admin)
    
    * feat(api): added action to make tos active
    
    ---------
    
    Co-authored-by: Gerald Iakobinyi-Pich <gerald@gitcoin.co>

--- api/tos/admin.py
+++ api/tos/admin.py
@@ -1,8 +1,12 @@
 # Register your models here.
+from typing import Any
 from django import forms
 from django.contrib import admin
+from django.http import HttpRequest
 from django_ace import AceWidget
 from scorer.scorer_admin import ScorerModelAdmin
+from django.contrib.auth import get_permission_codename
+from django.contrib import messages
 
 from .models import Tos, TosAcceptanceProof
 
@@ -36,10 +40,44 @@ class TosForm(forms.ModelForm):
 @admin.register(Tos)
 class TosAdmin(ScorerModelAdmin):
     form = TosForm
-    list_display = ("id", "type", "active", "created_at", "modified_at")
-    readonly_fields = ("created_at", "modified_at")
+    list_display = ("id", "type", "active", "final", "created_at", "modified_at")
+    readonly_fields = ["created_at", "modified_at"]
     search_fields = ("content", "type")
-    list_filter = ["active", "type"]
+    list_filter = ["active", "final", "type"]
+
+    actions = ["make_active"]
+
+    def get_readonly_fields(
+        self, request: HttpRequest, obj: Tos | None = ...
+    ) -> list[str] | tuple[Any, ...]:
+        ret = ["created_at", "modified_at"]
+        # An active object shall not be made inactive ...
+        if obj and obj.final:
+            ret += ["final", "content", "type"]
+        return ret
+
+    @admin.action(description="Make selected TOS active", permissions=["activate"])
+    def make_active(self, request, queryset):
+        count = len(queryset)
+        if count > 1:
+            self.message_user.e
+            messages.error(request, "You can only activate 1 tos")
+            return
+
+        prev_list = list(Tos.objects.filter(active=True))
+        if prev_list:
+            prev_active = prev_list[0]
+            prev_active.active = False
+            prev_active.save()
+        queryset.update(active=True)
+
+    def has_activate_permission(self, request):
+        """Does the user have the activate permission?"""
+
+        opts = self.opts
+        codename = get_permission_codename("activate", opts)
+        ret = request.user.has_perm("%s.%s" % (opts.app_label, codename))
+        return ret
 
 
 @admin.register(TosAcceptanceProof)
--- api/tos/tests/test_models.py
+++ api/tos/tests/test_models.py
@@ -4,6 +4,7 @@ from eth_account.account import Account
 from eth_account.messages import encode_defunct
 from tos.models import Tos, TosAcceptanceProof
 from web3 import Web3
+from django.db.utils import IntegrityError
 
 web3 = Web3()
 web3.eth.account.enable_unaudited_hdwallet_features()
@@ -15,7 +16,10 @@ class TestTosModelFunctions:
     def test_accept_tos(self):
         #  create the TOS object
         tos = Tos.objects.create(
-            type=Tos.TosType.IDENTITY_STAKING, active=True, content="Hello World !!!"
+            type=Tos.TosType.IDENTITY_STAKING,
+            active=True,
+            final=True,
+            content="Hello World !!!",
         )
 
         # get a message to sign
@@ -46,10 +50,82 @@ class TestTosModelFunctions:
 
     def test_has_any_accepted(self):
         tos = Tos.objects.create(
-            type=Tos.TosType.IDENTITY_STAKING, active=True, content="Hello World !!!"
+            type=Tos.TosType.IDENTITY_STAKING,
+            active=True,
+            final=True,
+            content="Hello World !!!",
         )
         assert tos.has_any_accepted() is False
         TosAcceptanceProof.objects.create(
             tos=tos, address="0x0", signature="0x0", nonce="12345"
         )
         assert tos.has_any_accepted() is True
+
+    def test_get_message_with_nonce(self):
+        tos = Tos.objects.create(
+            type=Tos.TosType.IDENTITY_STAKING, content="Hello World !!!"
+        )
+
+        # get_message_with_nonce shall only return if a tos
+        # with active=true and final=true exists
+        with pytest.raises(Tos.DoesNotExist):
+            tos.get_message_with_nonce(Tos.TosType.IDENTITY_STAKING)
+
+        tos = Tos.objects.create(
+            type=Tos.TosType.IDENTITY_STAKING, active=True, content="Hello World !!!"
+        )
+
+        with pytest.raises(Tos.DoesNotExist):
+            tos.get_message_with_nonce(Tos.TosType.IDENTITY_STAKING)
+
+        tos = Tos.objects.create(
+            type=Tos.TosType.IDENTITY_STAKING, final=True, content="Hello World !!!"
+        )
+
+        with pytest.raises(Tos.DoesNotExist):
+            tos.get_message_with_nonce(Tos.TosType.IDENTITY_STAKING)
+
+        tos = Tos.objects.create(
+            type=Tos.TosType.IDENTITY_STAKING,
+            active=True,
+            final=True,
+            content="Hello World !!!",
+        )
+
+        r = tos.get_message_with_nonce(Tos.TosType.IDENTITY_STAKING)
+
+        assert len(r) == 2
+
+    def test_constraint(self):
+        # Make sure only 1 active and final tos can exist
+        Tos.objects.create(
+            type=Tos.TosType.IDENTITY_STAKING,
+            active=True,
+            final=True,
+            content="Hello World !!!",
+        )
+
+        # This works
+        Tos.objects.create(
+            type=Tos.TosType.IDENTITY_STAKING,
+            active=False,
+            final=True,
+            content="Hello World !!!",
+        )
+
+        # This works as well
+        Tos.objects.create(
+            type=Tos.TosType.IDENTITY_STAKING,
+            active=True,
+            final=False,
+            content="Hello World !!!",
+        )
+
+        # This violates the constraint
+        with pytest.raises(IntegrityError):
+            Tos.objects.create(
+                type=Tos.TosType.IDENTITY_STAKING,
+                active=True,
+                final=True,
+                content="Hello World !!!",
+            )
