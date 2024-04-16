ca68cad329ad8835af59948731a304ebea6106c9
Lucian <lucian@gitcoin.co>
2024-04-16 13:29:23 -0600

    fix(indexer): when history query fails, propagate error (#571)
    * fix(indexer): when history query fails, propagate error
    
    * fix(indexer): reducing batch query size to 1k

--- indexer/src/legacy_staking_indexer.rs
+++ indexer/src/legacy_staking_indexer.rs
@@ -99,8 +99,9 @@ impl<'a> LegacyStakingIndexer<'a> {
         last_queried_block = last_queried_block + 1;
 
         // You can make eth_getLogs requests with up to a 2K block range and no limit on the response size
+        // Reducing to 1k because of occasional timeout issues
         while last_queried_block < current_block.as_u32() {
-            let next_block_range = last_queried_block.clone() + 2000;
+            let next_block_range = last_queried_block.clone() + 1000;
             let previous_events_query = id_staking_contract
                 .events()
                 .from_block(last_queried_block)
--- indexer/src/staking_indexer.rs
+++ indexer/src/staking_indexer.rs
@@ -49,7 +49,10 @@ impl<'a> StakingIndexer<'a> {
 
     pub async fn listen_with_timeout_reset(&self) -> Result<()> {
         loop {
-            let start_block = self.postgres_client.get_latest_block(self.chain_id, self.start_block).await?;
+            let start_block = self
+                .postgres_client
+                .get_latest_block(self.chain_id, self.start_block)
+                .await?;
             println!(
                 "Debug - Starting indexer for chain {} at block {}",
                 self.chain_id, start_block
@@ -88,7 +91,10 @@ impl<'a> StakingIndexer<'a> {
             // sleep for 15 minutes
             tokio::time::sleep(tokio::time::Duration::from_secs(900)).await;
 
-            let latest_logged_block = self.postgres_client.get_latest_block(self.chain_id, self.start_block).await?;
+            let latest_logged_block = self
+                .postgres_client
+                .get_latest_block(self.chain_id, self.start_block)
+                .await?;
 
             if latest_logged_block == timer_begin_block {
                 return Err(eyre::eyre!(
@@ -127,8 +133,9 @@ impl<'a> StakingIndexer<'a> {
         let mut last_queried_block: u64 = *query_start_block;
 
         // You can make eth_getLogs requests with up to a 2K block range and no limit on the response size
+        // Reducing to 1k because of occasional timeout issues
         while last_queried_block < current_block - 1 {
-            let query_end_block = min(last_queried_block + 2000, current_block - 1);
+            let query_end_block = min(last_queried_block + 1000, current_block - 1);
             let previous_events_query = id_staking_contract
                 .events()
                 .from_block(last_queried_block + 1)
@@ -141,15 +148,15 @@ impl<'a> StakingIndexer<'a> {
                     for (event, meta) in previous_events.iter() {
                         self.process_staking_event(&event, &meta, &client).await?;
                     }
+                    last_queried_block = query_end_block;
                 }
                 Err(err) => {
-                    eprintln!(
+                    return Err(eyre::eyre!(
                         "Error - Failed to query events: {}, {}, {:?}",
                         last_queried_block, query_end_block, err
-                    );
+                    ));
                 }
             }
-            last_queried_block = query_end_block;
         }
 
         eprintln!(
@@ -238,7 +245,7 @@ impl<'a> StakingIndexer<'a> {
         client: &Arc<ethers::providers::Provider<ethers::providers::Ws>>,
     ) -> Result<u64> {
         if let Ok(Some(block)) = client.get_block(block_number).await {
-            return Ok(block.timestamp.as_u64())
+            return Ok(block.timestamp.as_u64());
         }
         Err(eyre::eyre!(
             "Failed to fetch block timestamp for block {}",
@@ -253,7 +260,9 @@ impl<'a> StakingIndexer<'a> {
         tx_hash: &String,
         client: &Arc<ethers::providers::Provider<ethers::providers::Ws>>,
     ) -> Result<()> {
-        let timestamp = self.get_timestamp_for_block_number(block_number, client).await?;
+        let timestamp = self
+            .get_timestamp_for_block_number(block_number, client)
+            .await?;
 
         if let Err(err) = self
             .postgres_client
@@ -285,7 +294,9 @@ impl<'a> StakingIndexer<'a> {
         tx_hash: &String,
         client: &Arc<ethers::providers::Provider<ethers::providers::Ws>>,
     ) -> Result<()> {
-        let timestamp = self.get_timestamp_for_block_number(block_number, client).await?;
+        let timestamp = self
+            .get_timestamp_for_block_number(block_number, client)
+            .await?;
 
         if let Err(err) = self
             .postgres_client
