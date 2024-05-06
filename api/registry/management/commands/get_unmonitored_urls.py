37fe911d6c2d6084fc4ef822af3baf9d75dd20e0
Lucian Hymer <lucian@gitcoin.co>
2024-05-06 11:00:37 -0600

    feat(api): command to print unmonitored urls, added to infra

--- api/registry/management/commands/get_unmonitored_urls.py
+++ api/registry/management/commands/get_unmonitored_urls.py
@@ -1,4 +1,4 @@
-from django.core.management.base import BaseCommand
+from django.core.management.base import BaseCommand, CommandError
 from django.conf import settings
 from http.client import HTTPSConnection
 
@@ -13,8 +13,17 @@ IGNORED_PATH_ROOTS = [
 
 # Will ignore exact path matches
 IGNORED_URLS = [
-    "/",
+    "/registry/feature/openapi.json",
+    "/registry/feature/docs",
+    "/registry/feature/scorer/generic",
 ]
+
+# This should only be used to get a release out quickly when necessary, after
+# which the URL should be added to the hardcoded IGNORED_URLS above to be
+# ignored in the future
+if settings.IGNORE_UNMONITORED_URLS:
+    urls = [url.strip() for url in settings.IGNORE_UNMONITORED_URLS.split(",")]
+    IGNORED_URLS.extend(urls)
 
 
 class Command(BaseCommand):
@@ -30,14 +39,20 @@ class Command(BaseCommand):
         parser.add_argument(
             "--out",
             type=str,
-            help="Output file, json list of unmonitored URLs",
+            help="Output file, json list of unmonitored paths",
             required=True,
         )
         parser.add_argument(
             "--base-url",
             type=str,
-            help="Base URL for the site (to prepend to django URLs)",
+            help="Base URL for the site (uptime robot URLs will be filtered using this)",
             required=True,
+        )
+        parser.add_argument(
+            "--allow-paused",
+            type=bool,
+            help="Allow paused monitors to be considered monitored (default: False)",
+            default=False,
         )
 
     def handle(self, *args, **kwargs):
@@ -46,19 +61,21 @@ class Command(BaseCommand):
         self.stdout.write(f"kwargs   : {kwargs}")
 
         if not settings.UPTIME_ROBOT_READONLY_API_KEY:
-            self.stdout.write("UPTIME_ROBOT_READONLY_API_KEY is not set")
-            return
+            raise CommandError("UPTIME_ROBOT_READONLY_API_KEY is not set")
 
-        all_django_url_paths = self.get_all_urls(kwargs["urls"])
-        filtered_django_url_paths = self.filter_urls(all_django_url_paths)
-        django_urls = [kwargs["base_url"] + url for url in filtered_django_url_paths]
+        all_django_urls = self.get_all_urls(kwargs["urls"])
+        django_urls = self.filter_urls(all_django_urls)
 
-        self.stdout.write(f"Total URLs: {len(all_django_url_paths)}")
+        self.stdout.write(f"Total URLs: {len(all_django_urls)}")
+        self.stdout.write(f"Ignoring url path roots: {IGNORED_PATH_ROOTS}")
+        self.stdout.write(f"Ignoring urls: {IGNORED_URLS}")
         self.stdout.write(f"URLs to check: {len(django_urls)}")
 
-        monitored_urls = self.get_uptime_robot_urls()
+        monitored_urls = self.get_uptime_robot_urls(
+            base_url=kwargs["base_url"], allow_paused=kwargs["allow_paused"]
+        )
+        self.stdout.write(f"Allowing paused monitors: {kwargs['allow_paused']}")
         self.stdout.write(f"Uptime Robot URLs: {len(monitored_urls)}")
-
         self.stdout.write(f"Uptime robot URLs: {json.dumps(monitored_urls, indent=2)}")
 
         unmonitored_urls = []
@@ -93,27 +110,42 @@ class Command(BaseCommand):
     def convert_django_url_to_regex(self, url: str):
         # Have to do sub and then replace because re.sub interprets
         # the replacement string as an invalid group reference
-        url_regex = re.sub("<int:.*?>", "<int>", url).replace("<int>", "\\d+")
-        url_regex = re.sub("<str:.*?>", "<str>", url_regex).replace("<str>", "[^/]+")
-        return url_regex + "$"
 
-    def get_uptime_robot_urls(self):
+        # Sub integers
+        url_regex = re.sub("<int:.+?>", "<int>", url).replace("<int>", "\\d+")
+        # Sub strings
+        url_regex = re.sub("<str:.+?>", "<str>", url_regex).replace("<str>", "[^/]+")
+        # Sub untyped params
+        url_regex = re.sub("<[^:]+?>", "<str>", url_regex).replace("<str>", "[^/]+")
+
+        return url_regex + "(\\?|$)"
+
+    def get_uptime_robot_urls(self, base_url: str, allow_paused: bool):
         limit = 50
         offset = 0
 
-        urls = []
+        monitors = []
         while True:
             data = self.uptime_robot_monitors_request(limit, offset)
 
             total = data["pagination"]["total"]
-            urls.extend([monitor["url"] for monitor in data["monitors"]])
+            monitors.extend(data["monitors"])
 
-            if len(urls) >= total:
+            if len(monitors) >= total:
                 break
 
             offset += limit
 
-        return urls
+        if base_url.endswith("/"):
+            base_url = base_url[:-1]
+
+        urls = [
+            monitor["url"]
+            for monitor in monitors
+            if (allow_paused or monitor["status"] != 0)
+        ]
+
+        return [url.replace(base_url, "") for url in urls if url.startswith(base_url)]
 
     def uptime_robot_monitors_request(self, limit, offset):
         conn = HTTPSConnection("api.uptimerobot.com")
@@ -139,11 +171,16 @@ class Command(BaseCommand):
         return [entry["url"] for entry in urls_json]
 
     def filter_urls(self, urls):
+        # Remove urls that are just a path with a trailing slash, we
+        # don't use these in our api
+        filtered_urls = [url for url in urls if not re.match(r"^[^?]*/$", url)]
+
         filtered_urls = [
             url
-            for url in urls
+            for url in filtered_urls
             if not any(url.startswith("/" + root + "/") for root in IGNORED_PATH_ROOTS)
         ]
+
         filtered_urls = [
             url
             for url in filtered_urls
