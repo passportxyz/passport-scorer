9b8441ac70cdd6bae5f1acef6b4e8bf499ca8c48
larisa17 <larisabucur17@gmail.com>
2024-05-09 18:31:49 +0300

    add time logging

--- api/ceramic_cache/management/commands/base_cron_cmds.py
+++ api/ceramic_cache/management/commands/base_cron_cmds.py
@@ -1,32 +1,32 @@
-import requests
+import datetime
 from django.core.management.base import BaseCommand
-from django.conf import settings
+
 
 class BaseCronJobCmd(BaseCommand):
-    def handle_success(self):
-        """
-        Make a post request to UptimeRobot Heartbeat Url
-        """
-        if settings.UPTIME_ROBOT_HEARTBEAT_URL: 
-            # {"status":"ok","msg":"Heartbeat request received and processed successfully."}%   
-            response = requests.get(settings.UPTIME_ROBOT_HEARTBEAT_URL)
-            try: 
-                data = response.json()
-                if data.get("status", "not_ok") != "ok": 
-                    self.stderr.write(f"Error HeartBeat Process. UptimeRobot response: {response}");
-            except Exception as e: 
-                self.stderr.write(f"Error processing the uptimerobot response: {e}. Original response: {response}")
-        else: 
-            pass
+    def log_time(self, message, current_time):
+        formatted_time = current_time.strftime("%Y-%m-%d %H:%M:%S")
+        self.stdout.write(f"{message}: {formatted_time}")
 
     def handle(self, *args, **options):
         try:
+            start_time = datetime.datetime.now()
+            self.log_time("[TIMING] Start job", start_time)
             self.handle_cron_job(*args, **options)
-            self.handle_success()
         except Exception as e:
             # Handle any exceptions that occur during the command execution
             self.stderr.write(f"An error occurred: {e}")
+        finally:
+            end_time = datetime.datetime.now()
+            self.log_time("[TIMING] End job", end_time)
+            duration = end_time - start_time
+            hours, remainder = divmod(duration.total_seconds(), 3600)
+            minutes, seconds = divmod(remainder, 60)
+            self.stdout.write(
+                f"[TIMING] Job duration: {int(hours)} hours, {int(minutes)} minutes, {int(seconds)} seconds"
+            )
 
     def handle_cron_job(self, *args, **options):
         # This method will be overridden by child classes
-        raise NotImplementedError("handle_cron_job() method must be implemented by child classes")
+        raise NotImplementedError(
+            "handle_cron_job() method must be implemented by child classes"
+        )
