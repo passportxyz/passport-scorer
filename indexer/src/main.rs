3fc5485eb015c632feb35baf8d21e475489caed2
larisa17 <larisabucur17@gmail.com>
2024-05-27 15:35:22 +0300

    add OP sepolia

--- indexer/src/main.rs
+++ indexer/src/main.rs
@@ -28,6 +28,10 @@ async fn main() -> Result<()> {
             .parse::<Address>()
             .unwrap();
 
+        let contract_address_op_sepolia = get_env("STAKING_CONTRACT_ADDRESS_OP_SEPOLIA")
+            .parse::<Address>()
+            .unwrap();
+
         let contract_address_arbitrum_mainnet = get_env("STAKING_CONTRACT_ADDRESS_ARBITRUM_MAINNET")
             .parse::<Address>()
             .unwrap();
@@ -36,6 +40,7 @@ async fn main() -> Result<()> {
             run_legacy_indexer(postgres_client.clone()),
             run_ethereum_indexer(postgres_client.clone(), &contract_address_eth_mainnet),
             run_optimism_indexer(postgres_client.clone(), &contract_address_op_mainnet),
+            run_optimism_sepolia_indexer(postgres_client.clone(), &contract_address_op_sepolia),
             run_arbitrum_indexer(postgres_client.clone(), &contract_address_arbitrum_mainnet)
         ) {
             Ok(_) => {
@@ -124,4 +129,27 @@ async fn run_arbitrum_indexer(
     )
     .await?;
     arbitrum_staking_indexer.listen_with_timeout_reset().await
+}
+
+
+async fn run_optimism_sepolia_indexer(
+    postgres_client: PostgresClient,
+    contract_address: &Address,
+) -> Result<()> {
+    if get_env("INDEXER_OPTIMISM_SEPOLIA_ENABLED") != "true" {
+        return Ok(());
+    }
+
+    let op_sepolia_rpc_url = get_env("INDEXER_OPTIMISM_SEPOLIA_RPC_URL");
+    let op_sepolia_start_block = get_env("INDEXER_OPTIMISM_SEPOLIA_START_BLOCK")
+        .parse::<u64>()
+        .unwrap();
+    let op_sepolia_staking_indexer = StakingIndexer::new(
+        postgres_client,
+        &op_sepolia_rpc_url,
+        op_sepolia_start_block,
+        contract_address,
+    )
+    .await?;
+    op_sepolia_staking_indexer.listen_with_timeout_reset().await
 }
