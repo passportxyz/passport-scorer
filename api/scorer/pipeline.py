431f541cf9bb45f6b0f98551084774bdb17137c1
Tim <6887938+tim-schultz@users.noreply.github.com>
2024-09-12 11:53:32 -0600

    chore: update to passportxyz gmail login (#675)

--- api/scorer/pipeline.py
+++ api/scorer/pipeline.py
@@ -1,11 +1,16 @@
+"""
+This module contains functions for managing user groups and permissions in the context of social authentication.
+It specifically handles group assignments for users with "@passport.xyz" email addresses.
+"""
+
 from django.contrib.auth.models import Group
 
 
 def add_social_auth_user_to_group(**kwargs):
     user = kwargs["user"]
 
-    # The @gitcoin.co email requirement is also enforced at the google Oauth layer
-    if user.email.endswith("@gitcoin.co"):
+    # The @passport.xyz email requirement is also enforced at the google Oauth layer
+    if user.email.endswith("@passport.xyz"):
         admin_read_only_group = Group.objects.get(name="admin_read_only")
         user.groups.add(admin_read_only_group)
         user.is_staff = True
