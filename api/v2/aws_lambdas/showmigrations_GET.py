5638f9f36158d18dd5b025d9c2525aeb32c3517d
larisa17 <larisabucur17@gmail.com>
2024-11-19 17:05:36 +0200

    update show migrations V2

--- api/v2/aws_lambdas/showmigrations_GET.py
+++ api/v2/aws_lambdas/showmigrations_GET.py
@@ -1,41 +1,22 @@
-# import logging
 import os
-import subprocess
+from io import StringIO
 
-from aws_lambdas.utils import (
-    with_api_request_exception_handling,
-)
+from django.conf import settings
+from django.core.management import call_command
 
 
-@with_api_request_exception_handling
 # pylint: disable=unused-argument
 def handler(event, context):
+    """
+    Run show migrations command and return the output.
+    """
+    # Set the settings module if not already set
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "scorer.settings")
+    settings.configure()  # Necessary for Lambda environments
+
     try:
-        print("Running showmigrations ... ")
-        # print all environment variables
-        for name, value in os.environ.items():
-            print("{0}: {1}".format(name, value))
-        # print(" DATABASE_URL: ", event.get("DB_HOST"))
-        # Run the Django `showmigrations` command
-        # logger.info("Received event: %s", event)
-        # logger.info("Lambda context: %s", vars(context))
-        result = subprocess.run(
-            ["python", "manage.py", "showmigrations"],
-            stdout=subprocess.PIPE,
-            stderr=subprocess.PIPE,
-            text=True,
-        )
-
-        # Check if there was an error in the command
-        if result.returncode != 0:
-            # logger.error("Error running showmigrations: %s", result.stderr)
-            return {
-                "statusCode": 500,
-                "body": f"Error running showmigrations: {result.stderr}",
-            }
-        # logger.info("showmigrations output: %s", result.stdout)
-        # Return the output of `showmigrations`
-        return {"statusCode": 200, "body": result.stdout}
-
+        output = StringIO()
+        call_command("showmigrations", stdout=output)
+        return {"statusCode": 200, "body": output.getvalue()}
     except Exception as e:
-        return {"statusCode": 500, "body": f"An error occurred: {str(e)}"}
+        return {"statusCode": 500, "body": str(e)}
