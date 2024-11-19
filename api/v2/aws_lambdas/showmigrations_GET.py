0a81b6fd79623b85c2cd5c95a185861b7b9e1805
larisa17 <larisabucur17@gmail.com>
2024-11-19 20:14:06 +0200

    add debug message

--- api/v2/aws_lambdas/showmigrations_GET.py
+++ api/v2/aws_lambdas/showmigrations_GET.py
@@ -1,7 +1,5 @@
-import os
 from io import StringIO
 
-from django.conf import settings
 from django.core.management import call_command
 
 # pylint: disable=unused-import
@@ -13,13 +11,12 @@ def handler(event, context):
     """
     Run show migrations command and return the output.
     """
-    # Set the settings module if not already set
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "scorer.settings")
-    settings.configure()
 
     try:
+        print("Running showmigrations command")
         output = StringIO()
         call_command("showmigrations", stdout=output)
+        print("Done running showmigrations command")
         return {"statusCode": 200, "body": output.getvalue()}
     except Exception as e:
         return {"statusCode": 500, "body": str(e)}
