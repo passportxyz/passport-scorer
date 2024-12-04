20ac1197f1f968a4d3c8914ea5c5dcca30610add
larisa17 <larisabucur17@gmail.com>
2024-12-04 13:37:26 +0200

    add single function

--- api/v2/aws_lambdas/tests/test_stamp_score_get.py
+++ api/v2/aws_lambdas/tests/test_stamp_score_get.py
@@ -114,7 +114,10 @@ def test_successful_authentication(
             assert body["threshold"] == "20.00000"
 
             assert body["error"] is None
-            assert body["stamp_scores"] == {"Ens": "0.408", "Google": "0.525"}
+            assert body["stamps"] == {
+                "Ens": {"score": "0.408", "dedup": False, "expiration_date": None},
+                "Google": {"score": "0.525", "dedup": False, "expiration_date": None},
+            }
             # We just check that something != None was recorded for the last timestamp
             assert body["expiration_timestamp"] is not None
 
