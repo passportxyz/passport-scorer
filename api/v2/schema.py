4a0e7e1cd0eb7f622ffb8d7f02a43c95fd3bf046
larisa17 <larisabucur17@gmail.com>
2024-12-10 11:07:20 +0200

    update schema

--- api/v2/schema.py
+++ api/v2/schema.py
@@ -1,13 +1,17 @@
-from datetime import datetime
 from decimal import Decimal
 from typing import (
-    Any,
     Dict,
     Optional,
 )
 
 from ninja import Schema
 from pydantic import field_serializer
+
+
+class V2StampScoreResponse(Schema):
+    score: str
+    dedup: bool
+    expiration_date: str
 
 
 class V2ScoreResponse(Schema):
@@ -18,7 +22,7 @@ class V2ScoreResponse(Schema):
     expiration_timestamp: Optional[str]
     threshold: Decimal
     error: Optional[str]
-    stamps: Optional[Dict[str, Dict]]
+    stamps: Optional[Dict[str, V2StampScoreResponse]]
 
     @field_serializer("score")
     def serialize_score(self, score: Decimal, _info):
