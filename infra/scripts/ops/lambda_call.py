75937ca765431fb1b313249402f33e57c7b92d08
larisa17 <42570262+larisa17@users.noreply.github.com>
2025-04-03 09:56:31 +0300

    use session token if needed (#816)

--- infra/scripts/ops/lambda_call.py
+++ infra/scripts/ops/lambda_call.py
@@ -13,14 +13,25 @@ def main():
     aws_access_key = os.getenv("AWS_ACCESS_KEY_ID")
     aws_secret_key = os.getenv("AWS_SECRET_ACCESS_KEY")
     aws_region = os.getenv("AWS_REGION")
-
+    aws_session_token = os.getenv(
+        "AWS_SESSION_TOKEN"
+    )  # Optional, only if using temporary credentials
     if not all([aws_access_key, aws_secret_key, aws_region]):
         raise ValueError(
             "AWS credentials or region not properly set in environment variables"
         )
 
     # Create AWS4Auth instance for request signing
-    auth = AWS4Auth(aws_access_key, aws_secret_key, aws_region, "lambda")
+    if aws_session_token:
+        auth = AWS4Auth(
+            aws_access_key,
+            aws_secret_key,
+            aws_region,
+            "lambda",
+            session_token=aws_session_token,
+        )
+    else:
+        auth = AWS4Auth(aws_access_key, aws_secret_key, aws_region, "lambda")
 
     # Make the GET request to the Lambda Function URL
     response = requests.get(url, auth=auth)
