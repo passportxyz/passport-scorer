5b33f771b49147cd20342fc3a58dffc1d9186744
Lucian Hymer <lucian@gitcoin.co>
2025-05-28 06:58:26 -0600

    add tests

--- api/conftest.py
+++ api/conftest.py
@@ -4,3 +4,8 @@ import pytest
 @pytest.fixture(autouse=True)
 def enable_v2_api(settings):
     settings.FF_V2_API = "on"
+
+
+@pytest.fixture(autouse=True)
+def enable_multi_nullifier(settings):
+    settings.FF_MULTI_NULLIFIER = "on"
