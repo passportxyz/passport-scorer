89ee7967cb0e68d071ce52692b2973084e391f8e
Gerald Iakobinyi-Pich <nutrina9@gmail.com>
2025-06-06 10:44:12 +0300

    fix: adding back previously commented tests

--- api/v2/test/test_command_process_batch_address_upload.py
+++ api/v2/test/test_command_process_batch_address_upload.py
@@ -28,214 +28,223 @@ class MockPassportAnalysisResponse(Schema):
 
 
 class TestProcessBatchModelAddressUploads(TransactionTestCase):
-    # def test_process_pending_requests(self):
-    #     mock_s3_client = MagicMock()
-    #     mock_s3_client.get_object.return_value = {
-    #         "ContentLength": 100,
-    #         "Body": MagicMock(
-    #             read=lambda: StringIO(
-    #                 "Address\n0xd5680a051302d427efa518238fda2c848eebe714\n0xd5680a051302d427efa518238fda2c848eebe714\n0x0636F974D29d947d4946b2091d769ec6D2d415DE"
-    #             )
-    #             .getvalue()
-    #             .encode()
-    #         ),
-    #     }
+    def test_process_pending_requests(self):
+        # Create a mock for handle_get_analysis
+        mock_handle_get_analysis = AsyncMock(
+            return_value=MockPassportAnalysisResponse(
+                address="0x0",
+                details=MockPassportAnalysisDetails(
+                    models={
+                        "optimism": DetailedScoreModel(
+                            score=75,
+                            num_transactions=100,
+                            first_funder=None,
+                            first_funder_amount=None,
+                            first_funder_timestamp=None,
+                        )
+                    }
+                ),
+            )
+        )
 
-    #     # Create a mock for handle_get_analysis
-    #     mock_handle_get_analysis = AsyncMock(
-    #         return_value=MockPassportAnalysisResponse(
-    #             address="0x0",
-    #             details=MockPassportAnalysisDetails(
-    #                 models={
-    #                     "optimism": DetailedScoreModel(
-    #                         score=75,
-    #                         num_transactions=100,
-    #                         first_funder=None,
-    #                         first_funder_amount=None,
-    #                         first_funder_timestamp=None,
-    #                     )
-    #                 }
-    #             ),
-    #         )
-    #     )
+        with patch(
+            "registry.management.commands.process_batch_model_address_upload.BULK_MODEL_SCORE_RETRY_SLEEP",
+            0,
+        ):
+            with patch(
+                "registry.management.commands.process_batch_model_address_upload.handle_get_analysis",
+                mock_handle_get_analysis,
+            ):
+                # Create a pending request
+                trigger_file = ContentFile(
+                    json.dumps(
+                        {"action": "score", "batch_model_scoring_request_id": 123}
+                    ),
+                    name="score.json",
+                )
 
-    #     with patch(
-    #         "registry.management.commands.process_batch_model_address_upload.BULK_MODEL_SCORE_RETRY_SLEEP",
-    #         0,
-    #     ):
-    #         with patch(
-    #             "registry.management.commands.process_batch_model_address_upload.get_s3_client",
-    #             return_value=mock_s3_client,
-    #         ):
-    #             with patch(
-    #                 "registry.management.commands.process_batch_model_address_upload.handle_get_analysis",
-    #                 mock_handle_get_analysis,
-    #             ):
-    #                 good_request = BatchModelScoringRequest.objects.create(
-    #                     status=BatchRequestStatus.PENDING.value,
-    #                     s3_filename=f"test_file.csv",
-    #                     model_list=["model1", "model2"],
-    #                 )
+                input_file = ContentFile(
+                    """Address
+0xd5680a051302d427efa518238fda2c848eebe715
+0xd5680a051302d427efa518238fda2c848eebe714
+0x0636F974D29d947d4946b2091d769ec6D2d415DE""",
+                    name="addresses.json",
+                )
+                scoring_request = BatchModelScoringRequest.objects.create(
+                    status=BatchRequestStatus.PENDING.value,
+                    trigger_processing_file=trigger_file,
+                    input_addresses_file=input_file,
+                    model_list=["model1", "model2"],
+                )
 
-    #                 call_command("process_batch_model_address_upload")
+                # Call the command
+                call_command(
+                    "process_batch_model_address_upload",
+                    str(scoring_request.trigger_processing_file),
+                )
 
-    #                 updated_request = BatchModelScoringRequest.objects.get(
-    #                     id=good_request.id
-    #                 )
-    #                 self.assertEqual(
-    #                     updated_request.status,
-    #                     BatchRequestStatus.DONE.value,
-    #                     f"Expected status DONE, but got {good_request.status}",
-    #                 )
-    #                 self.assertEqual(
-    #                     updated_request.progress,
-    #                     100,
-    #                     f"Expected progress 100, but got {good_request.progress}",
-    #                 )
+                updated_request = BatchModelScoringRequest.objects.get(
+                    id=scoring_request.id
+                )
+                self.assertEqual(
+                    updated_request.status,
+                    BatchRequestStatus.DONE.value,
+                    f"Expected status DONE, but got {scoring_request.status}",
+                )
+                self.assertEqual(
+                    updated_request.progress,
+                    100,
+                    f"Expected progress 100, but got {scoring_request.progress}",
+                )
 
-    #                 expected_calls = 3  # 1 files * 3 addresses each
-    #                 self.assertEqual(
-    #                     mock_handle_get_analysis.call_count,
-    #                     expected_calls,
-    #                     f"Expected {expected_calls} calls to handle_get_analysis, but got {mock_handle_get_analysis.call_count}",
-    #                 )
-    #         assert mock_s3_client.get_object.call_count > 0
+                expected_calls = 3  # 1 files * 3 addresses each
+                self.assertEqual(
+                    mock_handle_get_analysis.call_count,
+                    expected_calls,
+                    f"Expected {expected_calls} calls to handle_get_analysis, but got {mock_handle_get_analysis.call_count}",
+                )
 
-    # def test_process_pending_requests_with_errors(self):
-    #     mock_s3_client = MagicMock()
-    #     mock_s3_client.get_object.return_value = {
-    #         "ContentLength": 100,
-    #         "Body": MagicMock(
-    #             read=lambda: StringIO(
-    #                 "Address\n0xd5680a051302d427efa518238fda2c848eebe715\n0xd5680a051302d427efa518238fda2c848eebe714\n0x0636F974D29d947d4946b2091d769ec6D2d415DE"
-    #             )
-    #             .getvalue()
-    #             .encode()
-    #         ),
-    #     }
+    def test_process_pending_requests_with_errors(self):
+        success_response = MockPassportAnalysisResponse(
+            address="0x0",
+            details=MockPassportAnalysisDetails(
+                models={
+                    "optimism": DetailedScoreModel(
+                        score=75,
+                        num_transactions=100,
+                        first_funder=None,
+                        first_funder_amount=None,
+                        first_funder_timestamp=None,
+                    )
+                }
+            ),
+        )
 
-    #     success_response = MockPassportAnalysisResponse(
-    #         address="0x0",
-    #         details=MockPassportAnalysisDetails(
-    #             models={
-    #                 "optimism": DetailedScoreModel(
-    #                     score=75,
-    #                     num_transactions=100,
-    #                     first_funder=None,
-    #                     first_funder_amount=None,
-    #                     first_funder_timestamp=None,
-    #                 )
-    #             }
-    #         ),
-    #     )
+        mock_handle_get_analysis = AsyncMock(
+            side_effect=[
+                Exception({"error": "error"}),
+                success_response,
+                success_response,
+                success_response,
+            ]
+        )
 
-    #     mock_handle_get_analysis = AsyncMock(
-    #         side_effect=[
-    #             Exception({"error": "error"}),
-    #             success_response,
-    #             success_response,
-    #             success_response,
-    #         ]
-    #     )
+        with patch(
+            "registry.management.commands.process_batch_model_address_upload.BULK_MODEL_SCORE_RETRY_SLEEP",
+            0,
+        ):
+            with patch(
+                "registry.management.commands.process_batch_model_address_upload.handle_get_analysis",
+                mock_handle_get_analysis,
+            ):
+                # Create a pending request
+                trigger_file = ContentFile(
+                    json.dumps(
+                        {"action": "score", "batch_model_scoring_request_id": 123}
+                    ),
+                    name="score.json",
+                )
 
-    #     with patch(
-    #         "registry.management.commands.process_batch_model_address_upload.BULK_MODEL_SCORE_RETRY_SLEEP",
-    #         0,
-    #     ):
-    #         with patch(
-    #             "registry.management.commands.process_batch_model_address_upload.get_s3_client",
-    #             return_value=mock_s3_client,
-    #         ):
-    #             with patch(
-    #                 "registry.management.commands.process_batch_model_address_upload.handle_get_analysis",
-    #                 mock_handle_get_analysis,
-    #             ):
-    #                 good_request = BatchModelScoringRequest.objects.create(
-    #                     status=BatchRequestStatus.PENDING.value,
-    #                     s3_filename=f"test_file.csv",
-    #                     model_list=["model1", "model2"],
-    #                 )
+                input_file = ContentFile(
+                    """Address
+0xd5680a051302d427efa518238fda2c848eebe715
+0xd5680a051302d427efa518238fda2c848eebe714
+0x0636F974D29d947d4946b2091d769ec6D2d415DE""",
+                    name="addresses.json",
+                )
+                scoring_request = BatchModelScoringRequest.objects.create(
+                    status=BatchRequestStatus.PENDING.value,
+                    trigger_processing_file=trigger_file,
+                    input_addresses_file=input_file,
+                    model_list=["model1", "model2"],
+                )
 
-    #                 call_command("process_batch_model_address_upload")
+                # Call the command
+                call_command(
+                    "process_batch_model_address_upload",
+                    str(scoring_request.trigger_processing_file),
+                )
 
-    #                 updated_request = BatchModelScoringRequest.objects.get(
-    #                     id=good_request.id
-    #                 )
-    #                 self.assertEqual(
-    #                     updated_request.status,
-    #                     BatchRequestStatus.DONE.value,
-    #                     f"Expected status DONE, but got {good_request.status}",
-    #                 )
-    #                 self.assertEqual(
-    #                     updated_request.progress,
-    #                     100,
-    #                     f"Expected progress 100, but got {good_request.progress}",
-    #                 )
+                updated_request = BatchModelScoringRequest.objects.get(
+                    id=scoring_request.id
+                )
+                self.assertEqual(
+                    updated_request.status,
+                    BatchRequestStatus.DONE.value,
+                    f"Expected status DONE, but got {scoring_request.status}",
+                )
+                self.assertEqual(
+                    updated_request.progress,
+                    100,
+                    f"Expected progress 100, but got {scoring_request.progress}",
+                )
 
-    #                 expected_calls = 4
-    #                 self.assertEqual(
-    #                     mock_handle_get_analysis.call_count,
-    #                     expected_calls,
-    #                     f"Expected {expected_calls} calls to handle_get_analysis, but got {mock_handle_get_analysis.call_count}",
-    #                 )
-    #         assert mock_s3_client.get_object.call_count > 0
+                expected_calls = 4
+                self.assertEqual(
+                    mock_handle_get_analysis.call_count,
+                    expected_calls,
+                    f"Expected {expected_calls} calls to handle_get_analysis, but got {mock_handle_get_analysis.call_count}",
+                )
 
-    # def test_process_pending_requests_with_all_errors(self):
-    #     mock_s3_client = MagicMock()
-    #     mock_s3_client.get_object.return_value = {
-    #         "ContentLength": 100,
-    #         "Body": MagicMock(
-    #             read=lambda: StringIO(
-    #                 "Address\n0xd5680a051302d427efa518238fda2c848eebe715\n0xd5680a051302d427efa518238fda2c848eebe714\n0x0636F974D29d947d4946b2091d769ec6D2d415DE"
-    #             )
-    #             .getvalue()
-    #             .encode()
-    #         ),
-    #     }
+    def test_process_pending_requests_with_all_errors(self):
+        mock_handle_get_analysis = AsyncMock(side_effect=Exception({"error": "error"}))
 
-    #     mock_handle_get_analysis = AsyncMock(side_effect=Exception({"error": "error"}))
+        with patch(
+            "registry.management.commands.process_batch_model_address_upload.BULK_MODEL_SCORE_RETRY_SLEEP",
+            0,
+        ):
+            with patch(
+                "registry.management.commands.process_batch_model_address_upload.handle_get_analysis",
+                mock_handle_get_analysis,
+            ):
+                # Create a pending request
+                trigger_file = ContentFile(
+                    json.dumps(
+                        {"action": "score", "batch_model_scoring_request_id": 123}
+                    ),
+                    name="score.json",
+                )
 
-    #     with patch(
-    #         "registry.management.commands.process_batch_model_address_upload.BULK_MODEL_SCORE_RETRY_SLEEP",
-    #         0,
-    #     ):
-    #         with patch(
-    #             "registry.management.commands.process_batch_model_address_upload.get_s3_client",
-    #             return_value=mock_s3_client,
-    #         ):
-    #             with patch(
-    #                 "registry.management.commands.process_batch_model_address_upload.handle_get_analysis",
-    #                 mock_handle_get_analysis,
-    #             ):
-    #                 good_request = BatchModelScoringRequest.objects.create(
-    #                     status=BatchRequestStatus.PENDING.value,
-    #                     s3_filename=f"test_file.csv",
-    #                     model_list=["model1", "model2"],
-    #                 )
+                input_file = ContentFile(
+                    """Address
+0xd5680a051302d427efa518238fda2c848eebe715
+0xd5680a051302d427efa518238fda2c848eebe714
+0x0636F974D29d947d4946b2091d769ec6D2d415DE""",
+                    name="addresses.json",
+                )
+                scoring_request = BatchModelScoringRequest.objects.create(
+                    status=BatchRequestStatus.PENDING.value,
+                    trigger_processing_file=trigger_file,
+                    input_addresses_file=input_file,
+                    model_list=["model1", "model2"],
+                )
 
-    #                 call_command("process_batch_model_address_upload")
+                # Call the command
+                call_command(
+                    "process_batch_model_address_upload",
+                    str(scoring_request.trigger_processing_file),
+                )
 
-    #                 updated_request = BatchModelScoringRequest.objects.get(
-    #                     id=good_request.id
-    #                 )
-    #                 self.assertEqual(
-    #                     updated_request.status,
-    #                     BatchRequestStatus.DONE.value,
-    #                     f"Expected status DONE, but got {good_request.status}",
-    #                 )
-    #                 self.assertEqual(
-    #                     updated_request.progress,
-    #                     100,
-    #                     f"Expected progress 100, but got {good_request.progress}",
-    #                 )
+                updated_request = BatchModelScoringRequest.objects.get(
+                    id=scoring_request.id
+                )
+                self.assertEqual(
+                    updated_request.status,
+                    BatchRequestStatus.DONE.value,
+                    f"Expected status DONE, but got {scoring_request.status}",
+                )
+                self.assertEqual(
+                    updated_request.progress,
+                    100,
+                    f"Expected progress 100, but got {scoring_request.progress}",
+                )
 
-    #                 expected_calls = 15
-    #                 self.assertEqual(
-    #                     mock_handle_get_analysis.call_count,
-    #                     expected_calls,
-    #                     f"Expected {expected_calls} calls to handle_get_analysis, but got {mock_handle_get_analysis.call_count}",
-    #                 )
-    #         assert mock_s3_client.get_object.call_count > 0
+                expected_calls = 15
+                self.assertEqual(
+                    mock_handle_get_analysis.call_count,
+                    expected_calls,
+                    f"Expected {expected_calls} calls to handle_get_analysis, but got {mock_handle_get_analysis.call_count}",
+                )
 
     def test_handle_error_during_processing(self):
         with patch(
@@ -248,7 +257,7 @@ class TestProcessBatchModelAddressUploads(TransactionTestCase):
                 name="score.json",
             )
 
-            # W ejust create an empty invalid file ...
+            # We just create an empty invalid file ...
             input_file = ContentFile(
                 """
 
@@ -256,7 +265,7 @@ class TestProcessBatchModelAddressUploads(TransactionTestCase):
 """,
                 name="addresses.json",
             )
-            scoringRequest = BatchModelScoringRequest.objects.create(
+            scoring_request = BatchModelScoringRequest.objects.create(
                 status=BatchRequestStatus.PENDING.value,
                 trigger_processing_file=trigger_file,
                 input_addresses_file=input_file,
@@ -266,7 +275,7 @@ class TestProcessBatchModelAddressUploads(TransactionTestCase):
             # Call the command
             call_command(
                 "process_batch_model_address_upload",
-                str(scoringRequest.trigger_processing_file),
+                str(scoring_request.trigger_processing_file),
             )
 
             # Refresh the request from the database
