788f0ed6dcc7d124806d28433866a95512b861f0
Lucian <lucian@gitcoin.co>
2025-10-16 08:53:42 -0600

    feat: Add Seasoned Passport OG and The Chosen One human points actions (#895)
    Adds two new list-based human points rules (SOG and TCO) similar to MetaMask OG, but without award limits. Addresses on SeasonedPassportOGs and TheChosenOnes lists can each claim their respective bonuses once.

--- api/registry/models.py
+++ api/registry/models.py
@@ -376,6 +376,8 @@ class HumanPoints(models.Model):
         HUMAN_TECH_BIOMETRIC = "HBI"
         METAMASK_OG = "MTA"
         METAMASK_OG_2 = "MM2"
+        SEASONED_PASSPORT_OG = "SOG"
+        THE_CHOSEN_ONE = "TCO"
 
     address = EthAddressField(db_index=True)
     action = models.CharField(max_length=3, choices=Action.choices, db_index=True)
