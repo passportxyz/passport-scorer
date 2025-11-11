c7378cb3892b40edf02a0f1a80d2680425f9f493
Lucian Hymer <lucian@gitcoin.co>
2025-11-11 18:16:34 +0000

    fix: formatting

--- api/registry/migrations/0059_add_stamp_metadata.py
+++ api/registry/migrations/0059_add_stamp_metadata.py
@@ -1,7 +1,7 @@
 # Generated manually for StampMetadata model
 
-from django.db import migrations, models
 import django.db.models.deletion
+from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
