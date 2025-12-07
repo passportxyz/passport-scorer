a817a3989d3ffeeac10b5deca967176360f93867
Muzzamil Mushtaq <muzzamilmushtaq1@gmail.com>
2025-12-07 16:46:04 +0500

    feat(scorer): add X stamp weight configuration (3.2 points)

--- api/scorer/settings/gitcoin_passport_weights.py
+++ api/scorer/settings/gitcoin_passport_weights.py
@@ -49,6 +49,7 @@ GITCOIN_PASSPORT_WEIGHTS = {
     "SnapshotProposalsProvider": "0.239",
     "TrustaLabs": "0.511",
     "TrustedCitizen": "4.009",
+    "X": "3.2",
     "ZkSyncEra": "0.606",
     "zkSyncScore#20": "1.67",
     "zkSyncScore#5": "1.67",
